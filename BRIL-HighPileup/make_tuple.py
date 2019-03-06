737071eea920fbc65409ecf6c52abe3d0e3bc3b6
Jose <jose.benitez@cern.ch>
2019-03-06 03:55:39 +0100

    7274

--- BRIL-HighPileup/make_tuple.py
+++ BRIL-HighPileup/make_tuple.py
@@ -18,22 +18,30 @@ sigma_plt = 11245.5/305  ##sigma from emmittance scan
 
 ##########################
 ####Fill 7358 (mu scan)
-h5file = tables.open_file('/brildata/vdmdata18/7358_1810260704_1810260726.hd5','r')
-BXLIST = [11,536,750,751,752,753,754,755,756,757,758,759,760,761,1644,1645,1646,1647,1648,1649,1650,1651,1652,1653,1654,1655]
-tmin=1540537800
-tmax=1540538550
-hist_pcc = TFile.Open( '/nfshome0/benitezj/vdmframework_final/VdmFramework/merged_7358.root')
-sigma_pcc  = 11245.5/5910000*23.311
+#h5file = tables.open_file('/brildata/vdmdata18/7358_1810260704_1810260726.hd5','r')
+#BXLIST = [11,536,750,751,752,753,754,755,756,757,758,759,760,761,1644,1645,1646,1647,1648,1649,1650,1651,1652,1653,1654,1655]
+#tmin=1540537800
+#tmax=1540538550
+#hist_pcc = TFile.Open( '/nfshome0/benitezj/vdmframework_final/VdmFramework/merged_7358.root')
+#sigma_pcc  = 11245.5/5910000*23.311
 
 #############################
 #### Fill 7274
-#h5file = tables.open_file('/brildata/vdmdata18/7274_1810102346_1810110021.hd5','r')
+h5file = tables.open_file('/brildata/vdmdata18/7274_1810102346_1810110021.hd5','r')
 #BXLIST = [62,63,64,65,66,67,68,69,70,71,117,118,119,120,121,122,123,124,125,126,196,197,198,199,200,201,202,203,204,205,251,252,253,254,255,256,257,258,259,260]
-#tmin=1539215350
-#tmax=1539215350+2000
+BXLeading = [62,117,196,251,306,385,440,495,574,629,684,767,822,901,956,1011,1090,1145,1200,1279,1334,1389,1468,1523,1578,1661,1716,1795,1850,1905,1984,2039,2094,2173,2228,2283,2362,2417,2472,2555,2610,2689,2744,2799,2878,2933,2988,3067,3122,3177,3256,3311,3366]
+NBXTrain = 5
+BXLIST =  array( 'i', 53*NBXTrain*[ 0 ] )
+for i in range(53):
+    for j in range(NBXTrain):
+        BXLIST[i*NBXTrain+j] = BXLeading[i] + j 
+print BXLIST
+tmin=1539215350
+tmax=1539215350+2000
 ###hist_pcc = TFile.Open( '/nfshome0/benitezj/vdmframework_final/VdmFramework/fromBrilcalc_delivered.root') #file from Yusuf but has wrong scaling
-#hist_pcc = TFile.Open( './fromBrilcalc_delivered.root') #file from Georgios email March 4, 1pm
-#sigma_pcc  = 11245.5/5910000
+hist_pcc = TFile.Open( './fromBrilcalc_delivered.root') #file from Georgios email March 4, 1pm
+sigma_pcc  = 11245.5/5910000
+
 
 
 print(h5file)
