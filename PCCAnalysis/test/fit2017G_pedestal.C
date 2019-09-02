ecae8e4ece7c5e264d21de42181fcbb9d2148725
Jose <jose.benitez@cern.ch>
2019-09-02 12:22:57 -0700

    plot

--- PCCAnalysis/test/fit2017G_pedestal.C
+++ PCCAnalysis/test/fit2017G_pedestal.C
@@ -2,9 +2,16 @@
 void fit2017G_pedestal(){
 
   gROOT->ProcessLine(".x BRILAnalysisCode/rootlogon.C");
-  
-  TFile CorrLumi("/Users/benitezj/analysis/PCC2017PbPb/306773_fix.root","read");
 
+  //TFile CorrLumi("/Users/benitezj/analysis/PCC2017PbPb/Run2017G_v3/306773.root");
+  //TFile CorrLumi("/Users/benitezj/analysis/PCC2017PbPb/Run2017G_v4/306773.root");
+  //TFile CorrLumi("/Users/benitezj/analysis/PCC2017PbPb/Run2017G_v3/306793.root");
+  TFile CorrLumi("/Users/benitezj/analysis/PCC2017PbPb/Run2017G_v4/306793.root");
+
+  gROOT->cd();
+  
+  TH1F* HC=0;
+  
   TF1 F("fit","[0]",0,4000);
   TF1 FL("fitL","[0]",0,4000);
 
@@ -22,7 +29,7 @@ void fit2017G_pedestal(){
 
 
       
-      TH1F * H=(TH1F*)(CorrLumi.Get(k->GetName())->Clone("H"));
+      TH1F * H=(TH1F*)(CorrLumi.Get(k->GetName())->Clone("HP"));
       TH1F * HL=(TH1F*)(CorrLumi.Get(k->GetName())->Clone("HL"));
       
       for(int b=1;b<=H->GetNbinsX();b++){
@@ -44,19 +51,22 @@ void fit2017G_pedestal(){
 
       GPedFrac.SetPoint(npts,npts,F.GetParameter(0)/FL.GetParameter(0));
       npts++;
+
+      //save the first histo for example fit
+      if(HC==0) HC=(TH1F*)(CorrLumi.Get(k->GetName())->Clone());
     }
   }
 
 
 
   TCanvas C;
-  
-  TH1F * HC=(TH1F*)CorrLumi.Get("CorrectedLumiAvg_306773_0_1_49");
-  HC->SetMarkerStyle(8);
-  HC->SetMarkerSize(0.5);
-  HC->GetYaxis()->SetTitle("PCC");
-  HC->GetXaxis()->SetTitle("bx id");
 
+  TString RawName=HC->GetName();
+  RawName.ReplaceAll("Corrected","Raw"); // "RawLumiAvg_306773_0_1_49");
+  TH1F * HR=(TH1F*)CorrLumi.Get(RawName.Data());
+  
+
+  ///Fit the pedestal for the example 
   TH1F * H=(TH1F*)HC->Clone("H");
   for(int b=1;b<=H->GetNbinsX();b++)
     if(H->GetBinContent(b)>20){ 
@@ -68,43 +78,49 @@ void fit2017G_pedestal(){
 
 
   C.SetLogy(1);
-
-
-  TH1F * HR=(TH1F*)CorrLumi.Get("RawLumiAvg_306773_0_1_49");
   HR->SetMarkerColor(4);
   HR->SetMarkerStyle(8);
   HR->SetMarkerSize(0.5);
+  HR->GetYaxis()->SetTitle("PCC");
+  HR->GetXaxis()->SetTitle("bx id");
+  HC->SetMarkerStyle(8);
+  HC->SetMarkerSize(0.5);
+  
   C.Clear();
-  HC->Draw("histp");
-  HR->Draw("histpsame");
+  HR->GetYaxis()->SetRangeUser(0.1,250);
+  HR->Draw("histp");
+  HC->Draw("histpsame");
   F.Draw("lsame");	
   C.Print("PCC2017PbPb/fit2017G_pedestal_RawAndCorr.gif");
+
+
+  /* C.Clear(); */
+  /* HR->GetXaxis()->SetRangeUser(400,800); */
+  /* HR->Draw("histp"); */
+  /* HC->Draw("histpsame"); */
+  /* F.Draw("lsame");	 */
+  /* C.Print("PCC2017PbPb/fit2017G_pedestal_RawAndCorr_zoom.gif"); */
+
+
   
-  C.Clear();//Zoom
-  HC->GetXaxis()->SetRangeUser(700,900);
-  HC->Draw("histp");
-  HR->Draw("histpsame");
-  C.Print("PCC2017PbPb/fit2017G_pedestal_RawAndCorr_zoom.gif");
-
-
 
   C.SetLogy(0);
   C.Clear();
-  GPedestal.GetYaxis()->SetRangeUser(0,2);
+  GPedestal.GetYaxis()->SetRangeUser(0,10);
   GPedestal.GetYaxis()->SetTitle("pedestal");
   GPedestal.GetXaxis()->SetTitle("lumi section block");
   GPedestal.Draw("ap");
   C.Print("PCC2017PbPb/fit2017G_pedestal_vs_lsblock.gif");
 
   C.Clear();
-  GLumi.GetYaxis()->SetRangeUser(0,90);
+  GLumi.GetYaxis()->SetRangeUser(0,180);
   GLumi.GetYaxis()->SetTitle("PCC");
   GLumi.GetXaxis()->SetTitle("lumi section block");
   GLumi.Draw("ap");
   C.Print("PCC2017PbPb/fit2017G_pedestal_lumi_vs_lsblock.gif");
 
   C.Clear();
-  GPedFrac.GetYaxis()->SetRangeUser(0,0.05);
+  GPedFrac.GetYaxis()->SetRangeUser(-0.001,0.15);
   GPedFrac.GetYaxis()->SetTitle("Pedestal / PCC");
   GPedFrac.GetXaxis()->SetTitle("lumi section block");
   GPedFrac.Draw("ap");
