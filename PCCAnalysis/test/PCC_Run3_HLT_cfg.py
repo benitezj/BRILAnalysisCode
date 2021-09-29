fac051a86ce5fe06607e1639925c979a014ea251
Jose <jose.benitez@cern.ch>
2021-09-29 02:09:03 +0200

    updated cfg

--- PCCAnalysis/test/PCC_Run3_HLT_cfg.py
+++ PCCAnalysis/test/PCC_Run3_HLT_cfg.py
@@ -1,16 +1,15 @@
 #########################
+## standalone config to test PCC modules
 #########################
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("ALCAPCC")
+process = cms.Process("PCC")
 
 process.source = cms.Source("PoolSource",
-#fileNames = cms.untracked.vstring('file:/eos/cms/store/data/Run2015D/AlCaLumiPixels/ALCARECO/LumiPixels-PromptReco-v4/000/260/039/00000/1CF2A210-5B7E-E511-8F4F-02163E014145.root')
-#fileNames = cms.untracked.vstring('file:/eos/cms/store/data/Run2018D/ZeroBias/RAW/v1/000/323/841/00000/FDCDC8F6-35E7-FE49-9D99-4D7CA208D807.root')
-fileNames = cms.untracked.vstring('file:/eos/cms/store/data/Run2016H/AlCaLumiPixels0/RAW/v1/000/281/572/00000/BAE55739-5384-E611-8E2D-02163E011FA9.root')
+#fileNames = cms.untracked.vstring('file:/eos/cms/store/data/Run2016H/AlCaLumiPixels0/RAW/v1/000/281/601/00000/2C65B0FE-6D8C-E611-8CB6-FA163E00F43A.root')
+fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/b/benitezj/public/BRIL/PCC/Run3Dev/Run2018D-AlCaLumiPixels-RAW-323702-D3FCD0FC-6328-B24E-AD3D-C22C55B968DD.root')
 )
 
-# -- number of events
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
 )
@@ -18,21 +17,36 @@ process.maxEvents = cms.untracked.PSet(
 
 
 ############################
-### DB
+### DB inputs needed: Pix cabling map (RAW->Digi) and Geometry (Digi->Reco)
+#process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+#process.GlobalTag.globaltag = '101X_dataRun2_Prompt_v9'
+#process.GlobalTag.globaltag = '106X_dataRun2_v10'
+#process.GlobalTag.DumpStat = cms.untracked.bool( False )
+#process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.AlCaRecoStreams_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = '101X_dataRun2_Prompt_v9'
-process.GlobalTag.DumpStat = cms.untracked.bool( False )
-process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
 
 ############################
-#HLT filter for PCC
-process.ALCARECOHltFilterForPCC = cms.EDFilter("HLTHighLevel",
-    HLTPaths = cms.vstring("*Random*"),
-    eventSetupPathsKey = cms.string(""),
-    TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
-    andOr = cms.bool(True),
-    throw = cms.bool(False)
-)
+#HLT filter for PCC  -> Not needed I think
+#process.ALCARECOHltFilterForPCC = cms.EDFilter("HLTHighLevel",
+#    HLTPaths = cms.vstring("*Random*"),
+#    eventSetupPathsKey = cms.string(""),
+#    TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
+#    andOr = cms.bool(True),
+#    throw = cms.bool(False)
+#)
 
 
 
@@ -63,22 +77,39 @@ process.siPixelClustersForLumi = cms.EDProducer("SiPixelClusterProducer",
 )
 
 ################################
-#RECO->PCC 
+#RECO->ALCAPCC 
 process.alcaPCCEventProducer = cms.EDProducer("AlcaPCCEventProducer",
     pixelClusterLabel = cms.InputTag("siPixelClustersForLumi"),
-    #Mod factor to count lumi and the string to specify output 
     trigstring = cms.untracked.string("") 
 )
 
-#process.seqALCARECOPromptCalibProdPCC = cms.Sequence(process.alcaPCCEventProducer)
-#process.seqALCARECOPromptCalibProdPCC = cms.Sequence(process.ALCARECOHltFilterForPCC+process.alcaPCCEventProducer)
-#process.seqALCARECOPromptCalibProdPCC = cms.Sequence(process.ALCARECOHltFilterForPCC+process.siPixelDigisForLumi+process.siPixelClustersForLumi+process.alcaPCCEventProducer)
-process.seqALCARECOPromptCalibProdPCC = cms.Sequence(process.siPixelDigisForLumi+process.siPixelClustersForLumi+process.alcaPCCEventProducer)
-process.pathALCARECOPromptCalibProdPCC = cms.Path(process.seqALCARECOPromptCalibProdPCC)
+################################
+##ALCAPCC->ALCARECO
+process.alcaPCCIntegrator = cms.EDProducer("AlcaPCCIntegrator",
+    AlcaPCCIntegratorParameters = cms.PSet(
+        inputPccLabel = cms.string("alcaPCCEventProducer"),
+        trigstring = cms.untracked.string(""),
+        ProdInst = cms.string("")
+    ),
+)
+
+################################
+##ALCARECO->csv
+process.rawPCCProd = cms.EDProducer("RawPCCProducer",
+    RawPCCProducerParameters = cms.PSet(
+        inputPccLabel = cms.string("alcaPCCIntegrator"),
+        ProdInst = cms.string(""),
+        outputProductName = cms.untracked.string("lumiInfo"),
+        ApplyCorrections=cms.untracked.bool(False),
+        saveCSVFile=cms.untracked.bool(True),
+        modVeto=cms.vint32(),
+        OutputValue = cms.untracked.string("Average"),
+    )
+)
 
 
 #################################
-#OutPath products
+# OutPath products
 process.ALCARECOStreamPromptCalibProdPCC = cms.OutputModule("PoolOutputModule",
     SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring('pathALCARECOPromptCalibProdPCC')
@@ -88,14 +119,26 @@ process.ALCARECOStreamPromptCalibProdPCC = cms.OutputModule("PoolOutputModule",
         filterName = cms.untracked.string('PromptCalibProdPCC')
     ),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
-    fileName = cms.untracked.string('ALCAPCC.root'),
+    fileName = cms.untracked.string('PCC.root'),
     outputCommands = cms.untracked.vstring('drop *', 
+                                           #'keep *_hltFEDSelectorLumiPixels_*_*',
+                                           #'keep *_siPixelDigisForLumi_*_*',
+                                           'keep *_siPixelClustersForLumi_*_*',
                                            'keep *_alcaPCCEventProducer_*_*',
-                                           'keep *_MEtoEDMConvertSiStrip_*_*')
+                                           'keep *_alcaPCCIntegrator_*_*',
+                                           'keep *_rawPCCProd_*_*'
+                                       )
 )
 
 
+
+
+####################
+### cms sequences/paths
+process.seqALCARECOPromptCalibProdPCC = cms.Sequence(process.siPixelDigisForLumi+process.siPixelClustersForLumi+process.alcaPCCEventProducer+process.alcaPCCIntegrator+process.rawPCCProd)
+process.pathALCARECOPromptCalibProdPCC = cms.Path(process.seqALCARECOPromptCalibProdPCC)
 process.ALCARECOStreamPromptCalibProdOutPath = cms.EndPath(process.ALCARECOStreamPromptCalibProdPCC)
+process.schedule = cms.Schedule(*[ process.pathALCARECOPromptCalibProdPCC, process.ALCARECOStreamPromptCalibProdOutPath ])
 
 
 #################################################
@@ -106,11 +149,11 @@ process.MessageLogger = cms.Service("MessageLogger",
         ),
         FwkReport = cms.untracked.PSet(
             limit = cms.untracked.int32(10000000),
-            reportEvery = cms.untracked.int32(100000)
+            reportEvery = cms.untracked.int32(1000)
         ),
         FwkSummary = cms.untracked.PSet(
             limit = cms.untracked.int32(10000000),
-            reportEvery = cms.untracked.int32(100)
+            reportEvery = cms.untracked.int32(1000)
         ),
         INFO = cms.untracked.PSet(
             limit = cms.untracked.int32(0)
@@ -127,16 +170,11 @@ process.MessageLogger = cms.Service("MessageLogger",
         statisticsThreshold = cms.untracked.string('WARNING')
     ),
     debugModules = cms.untracked.vstring(),
-    default = cms.untracked.PSet(
-
-    ),
+    default = cms.untracked.PSet(),
     suppressDebug = cms.untracked.vstring(),
     suppressInfo = cms.untracked.vstring(),
     suppressWarning = cms.untracked.vstring()
 )
 
-#added line for additional output summary `
+
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
-
-
-process.schedule = cms.Schedule(*[ process.pathALCARECOPromptCalibProdPCC, process.ALCARECOStreamPromptCalibProdOutPath ])
