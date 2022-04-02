bdc4950d42716e1a0910da82f7d927eb7645a0a8
ashish <asehrawat1991@gmail.com>
2022-04-02 03:03:02 +0200

    std map ls.dat file

--- PCCAnalysis/plots/mappinglsdotdat.C
+++ PCCAnalysis/plots/mappinglsdotdat.C
@@ -3,7 +3,7 @@
 #include <iostream>
 
 void mappinglsdotdat() {  
-
+  
   std::ifstream myfile("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/src/BRIL/PCC_lumi_oldveto_Run2018A/Run2018A/ls.dat"); 
   
   if (!myfile) { 
@@ -19,29 +19,29 @@ void mappinglsdotdat() {
   // Read the file and load the data:
   int run, ls;
   float count;
- 
+  
   while (std::getline(myfile, line)){
     
     std::getline(myfile, line);
     std::stringstream iss(line);
     iss>>run>>ls>>count;
-      
-    cluster_count[run][ls]=count;
-    //std::cout<<run<<" "<<ls<<"  "<<cluster_count[run][ls]<<std::endl;
+    if(run==315257) {      
+      cluster_count[run][ls]=count;
+      //std::cout<<run<<" "<<ls<<"  "<<cluster_count[run][ls]<<std::endl;
+    }
+    
   }
   
-
   for(auto & outer_map_pair : cluster_count) {
-    //cout << outer_map_pair.first << " ";
+    cout << outer_map_pair.first << " ";
     for(auto & inner_map_pair : outer_map_pair.second) {
       cout << "old "<<outer_map_pair.first << " "<<inner_map_pair.first << " " << inner_map_pair.second<<endl;
     }
   }
-
-
+  
+  
   myfile.close();  
   
-
   std::ifstream myfile1("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/src/BRIL/PCC_lumi_newveto_2018A/Run2018A_ZB/ls.dat"); 
   
   if (!myfile1) { 
@@ -57,25 +57,27 @@ void mappinglsdotdat() {
   // Read the file and load the data:
   int run1, ls1;
   float count1;
- 
+  
   while (std::getline(myfile1, line1)){
     
     std::getline(myfile1, line1);
     std::stringstream iss1(line1);
     iss1>>run1>>ls1>>count1;
-   
-    cluster_count1[run1][ls1]=count1;
+    
+    if(run1==315257) {   
+      cluster_count1[run1][ls1]=count1;
     //std::cout<<run<<" "<<ls<<"  "<<cluster_count[run][ls]<<std::endl;
+    }
+    
   }
   
-
   for(auto & outer_map_pair1 : cluster_count1) {
-    //cout << outer_map_pair1.first << "  ";
+    cout << outer_map_pair1.first << "  ";
     for(auto & inner_map_pair1 : outer_map_pair1.second) {
       cout << "new "<<outer_map_pair1.first << "  "<< inner_map_pair1.first << "  " << inner_map_pair1.second << endl;
     }
   }
-
+  
   myfile1.close();  
   
 
