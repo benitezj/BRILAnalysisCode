547a521826d63f5609e192d2422be3349a94c253
ashish <asehrawat1991@gmail.com>
2022-04-03 10:49:46 +0200

    PCC per run

---
+++ PCCAnalysis/plots/PCCperrun.C
@@ -0,0 +1,69 @@
+void PCCperrun() {
+
+  TString Path = "/eos/user/a/asehrawa/BRIL-new/";
+  
+  TCanvas C("C");
+  C.cd();
+  
+  //TGraph * g = new TGraph("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/src/BRIL/PCC_lumi_newveto_2018A/Run2018A_ZB/runs.dat");
+  //TGraph * g = new TGraph("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/src/BRIL/PCC_lumi_newveto_2018B/Run2018B_ZB/runs.dat");
+  TGraph * g = new TGraph("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/src/BRIL/PCC_lumi_newveto_2018C/Run2018C_ZB/runs.dat");
+
+
+  TH1* h = new TH1D("h", "PCC per run (new veto)", 200, 0.0, 30000); 
+  auto nPoints = g->GetN(); 
+  for(int i=0; i < nPoints; ++i) {
+    double x,y;
+    g->GetPoint(i, x, y);
+    h->Fill(x,y); 
+  }
+
+
+
+  g->SetMarkerStyle(20);
+  g->SetMarkerSize(0.5);
+  g->SetMarkerColor(1);
+  g->SetLineColor(1);
+  g->SetTitle("PCC per run");
+  g->GetXaxis()->SetTitle("run number");
+  g->GetYaxis()->SetTitle("PCC");
+  g->Draw("ap");
+  //C.Print(Path+"PCCperrun_newveto_RunA"+".png");
+  
+ 
+  //TGraph * g1 = new TGraph("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/src/BRIL/PCC_lumi_oldveto_Run2018A/Run2018A/runs.dat");
+  //TGraph * g1 = new TGraph("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/src/BRIL/PCC_lumi_oldveto_Run2018B/Run2018B/runs.dat");
+  TGraph * g1 = new TGraph("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/src/BRIL/PCC_lumi_oldveto_Run2018C/Run2018C/runs.dat");
+
+
+  TH1* h1 = new TH1D("h1", "PCC per run (old veto)", 200, 0.0, 30000); 
+  auto nPoints1 = g1->GetN();                                                                        
+  for(int j=0; j < nPoints1; ++j) {
+    double x1,y1;
+    g1->GetPoint(j, x1, y1);
+    h1->Fill(x1,y1);                                                                                                               
+  }
+  
+
+
+  g1->SetMarkerStyle(20);
+  g1->SetMarkerSize(0.5);
+  g1->SetMarkerColor(2);
+  g1->SetLineColor(2);
+  g1->SetTitle("PCC per run (old veto)");
+  g1->GetXaxis()->SetTitle("run number");
+  g1->GetYaxis()->SetTitle("PCC");
+  g1->Draw("psame");
+
+
+  auto* legend = new TLegend(0.77,0.77,0.89,0.89);                                                                                           
+  legend->AddEntry(g,"new veto","l");
+  legend->AddEntry(g1,"old veto","l");  
+  legend->SetFillColor(0);
+  legend->SetLineColor(0);
+  legend->SetFillColor(0);
+  legend->Draw("same");    
+
+  C.Print(Path+"PCCperrun_old_new_veto_RunC"+".png");
+  
+}
