96184cae3b402857a3feb94c2f1394d3e7513db8
ashish <asehrawat1991@gmail.com>
2022-04-13 08:10:29 +0200

    PCC per ls

--- PCCAnalysis/plots/comparelsdotdat_express.C
+++ PCCAnalysis/plots/comparelsdotdat_express.C
@@ -37,13 +37,24 @@ void comparelsdotdat_express() {
     iss>>run>>ls>>count;
   
     graph->SetPoint(Lumicounter, Lumicounter, count);
-    cout<<"new "<<run<<"  "<<ls<<"  "<<Lumicounter<<"  "<<count<<endl;
+    //cout<<"new "<<run<<"  "<<ls<<"  "<<Lumicounter<<"  "<<count<<endl;
     
     Lumicounter++;
   
   }
 
   myfile.close(); 
+
+
+
+  TH1* h = new TH1D("h", "new/old", 59708, 0.0, 30000); 
+  auto nPoints = graph->GetN(); 
+  for(int i=0; i < nPoints; ++i) {
+    double x,y;
+    graph->GetPoint(i, x, y);
+    h->Fill(x,y); 
+  }
+  
 
 
   TString inpath1="/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/src/BRIL/PCC_lumi_oldveto_Run2018A/Run2018A";
@@ -74,15 +85,26 @@ void comparelsdotdat_express() {
     iss1>>run1>>ls1>>count1;
   
     graph1->SetPoint(Lumicounter1, Lumicounter1, count1);
-    cout<<"old "<<run1<<"  "<<ls1<<"  "<<Lumicounter1<<"  "<<count1<<endl;
+    //cout<<"old "<<run1<<"  "<<ls1<<"  "<<Lumicounter1<<"  "<<count1<<endl;
     
     Lumicounter1++;
   
   }
 
   myfile1.close(); 
+
+
+  TH1* h1 = new TH1D("h1", "PCC count per ls (old)", 59708, 0.0, 30000); 
+  auto nPoints1 = graph1->GetN();                                                                        
+  for(int j=0; j < nPoints1; ++j) {
+    double x1,y1;
+    graph1->GetPoint(j, x1, y1);
+    h1->Fill(x1,y1);                                                                                                               
+  }
+
    
   TCanvas C;
+  //C.SetLogy();
   
   graph->SetTitle("PCC per lumi section (new veto)");
   graph->GetXaxis()->SetTitle("lumi section");
@@ -92,7 +114,7 @@ void comparelsdotdat_express() {
   graph->SetMarkerStyle(8); 
   graph->Draw("AP");  
 
-  C.Print(Path+"/compare_lsdotdat_Run2018A_newveto.root");
+  C.Print(Path+"/compare_lsdotdat_Run2018A_newveto.png");
   
   graph1->SetTitle("PCC per lumi section (old veto)");
   graph1->GetXaxis()->SetTitle("lumi section");
@@ -102,6 +124,23 @@ void comparelsdotdat_express() {
   graph1->SetMarkerStyle(8);
   graph1->Draw("AP");
 
-  C.Print(Path+"/compare_lsdotdat_Run2018A_oldveto.root");
+  C.Print(Path+"/compare_lsdotdat_Run2018A_oldveto.png");
+
+  h->GetYaxis()->SetRangeUser(0, 3);
+  h->GetXaxis()->SetTitle("lumi section");
+  h->GetYaxis()->SetTitle("new/old");
+  h->SetMarkerColor(1);
+  h->SetMarkerSize(0.5);
+  h->SetMarkerStyle(8);
+
+  if(&y1!=0){
+  h->Divide(h1);
+  }
+  h->Draw("histp");
+  
+  C.Print(Path+"/compare_lsdotdat_hist_Run2018A_newveto_ratio.png");
+
+  //h1->Draw("histp");
+  //C.Print(Path+"/compare_lsdotdat_hist_Run2018A_oldveto.png");
 
 }
