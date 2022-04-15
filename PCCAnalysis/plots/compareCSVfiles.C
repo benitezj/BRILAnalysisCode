dafb071c0241fa974dd3e552b3c95e9af5dc0997
Jose <jose.benitez@cern.ch>
2022-04-15 14:47:53 -0700

    plot

--- PCCAnalysis/plots/compareCSVfiles.C
+++ PCCAnalysis/plots/compareCSVfiles.C
@@ -1,26 +1,25 @@
 #include "globals.h"
 
 
-void compareCSVfiles(TString newF, TString refF){
-  gROOT->ProcessLine(".x BRILAnalysisCode/rootlogon.C");
+void compareCSVfiles(TString path,TString newF, TString refF){
+  gROOT->ProcessLine(".x BRILAnalysisCode/PCCAnalysis/plots/rootlogon.C");
 
   bool plotbx=0;
-  
 
   TGraph LumiNew;
   TGraph LumiRef;
   TGraph LumiRatio;
 
 
-  ifstream newfile((newF).Data());
+  ifstream newfile((path+"/"+newF).Data());
   if (!newfile.is_open()){
-    std::cout << "Unable to open runs.dat in "<<newF.Data()<<std::endl;
+    std::cout << "Unable to open  "<<newF.Data()<<std::endl;
     return;
   }
 
-  ifstream reffile((refF).Data());
+  ifstream reffile((path+"/"+refF).Data());
   if (!reffile.is_open()){
-    std::cout << "Unable to open runs.dat in "<<refF.Data()<<std::endl;
+    std::cout << "Unable to open "<<refF.Data()<<std::endl;
     return;
   }
 
@@ -34,6 +33,7 @@ void compareCSVfiles(TString newF, TString refF){
   int refbx=0;
   float Lumi = 0.;
   float refLumi = 0.;
+  float maxLumi=0.;
   int counter=0;
   while (std::getline(newfile,line)){
     std::stringstream iss(line);
@@ -51,7 +51,7 @@ void compareCSVfiles(TString newF, TString refF){
     std::stringstream lumiiss(token);
     lumiiss>>Lumi;
 
-    //cout<<newF<<" "<<ls<<" "<<Lumi<<": "<<endl;
+    //cout<<newF<<" "<<ls<<" "<<Lumi<<endl;
     LumiNew.SetPoint(LumiNew.GetN(),LumiNew.GetN(),Lumi);
 
     //read the   reference
@@ -71,9 +71,12 @@ void compareCSVfiles(TString newF, TString refF){
     std::stringstream reflumiiss(reftoken);
     reflumiiss>>refLumi;
 
-    //cout<<refF<<" "<<refls<<" "<<refLumi<<": "<<endl;
+    //cout<<refF<<" "<<refls<<" "<<refLumi<<endl;
     LumiRef.SetPoint(LumiRef.GetN(),LumiRef.GetN(),refLumi);
-
+    
+    if(maxLumi<Lumi) maxLumi = Lumi;
+    if(maxLumi<refLumi) maxLumi = refLumi;
+    
     //fill the ratio
     if(run==refrun && ls==refls){
       if(refLumi>0)
@@ -81,7 +84,6 @@ void compareCSVfiles(TString newF, TString refF){
       else if(Lumi==0.)
 	LumiRatio.SetPoint(LumiRatio.GetN(),LumiRatio.GetN(),1);
       else LumiRatio.SetPoint(LumiRatio.GetN(),LumiRatio.GetN(),-1);
-      
     }
 
 
@@ -96,10 +98,7 @@ void compareCSVfiles(TString newF, TString refF){
 	std::stringstream refbiss(reftoken);
 	refbiss>>refLumi;
 	
-	if(refLumi>0) 
-	  //cout<<b<<":"<<Lumi<<"/"<<refLumi<<"="<<Lumi/refLumi<<endl;
       }
-      //cout<<endl;
     }
 
   }
@@ -111,11 +110,37 @@ void compareCSVfiles(TString newF, TString refF){
   gStyle->SetOptStat(0);
 
   TCanvas C;
-  LumiRatio.GetYaxis()->SetRangeUser(0,2);
+
+  C.Clear();
+  LumiNew.GetYaxis()->SetRangeUser(-0.1*maxLumi,1.1*maxLumi);
+  LumiNew.SetMarkerStyle(3);
+  LumiNew.SetMarkerSize(0.2);
+  LumiNew.SetMarkerColor(4);
+  LumiNew.GetXaxis()->SetTitle("lumi section");
+  LumiNew.GetYaxis()->SetTitle("PCC");
+  LumiNew.Draw("ap");
+
+  LumiRef.SetMarkerStyle(3);
+  LumiRef.SetMarkerSize(0.2);
+  LumiRef.SetMarkerColor(2);
+  LumiRef.Draw("psame");
+  
+  TLatex text;
+  text.SetTextSize(0.025);
+  text.SetTextColor(4);
+  text.DrawLatexNDC(0.05,0.97,newF.Data());
+  text.SetTextColor(2);
+  text.DrawLatexNDC(0.05,0.94,refF.Data());
+  C.Print(path+"/compareCSVfiles.png");
+  
+  C.Clear();
+  LumiRatio.GetYaxis()->SetRangeUser(-0.01,2);
+  LumiRatio.GetXaxis()->SetTitle("lumi section");
+  LumiRatio.GetYaxis()->SetTitle("ratio");
   LumiRatio.SetMarkerStyle(8);
   LumiRatio.SetMarkerSize(0.5);
   LumiRatio.Draw("ap");
-  C.Print("compareCSVfiles_ratio.png");
+  C.Print(path+"/compareCSVfiles_ratio.png");
 
   gROOT->ProcessLine(".q");
 }
