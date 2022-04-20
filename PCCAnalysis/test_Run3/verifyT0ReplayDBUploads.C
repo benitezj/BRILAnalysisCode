d6ee4285294afc7d6dc47b13c15e216335387610
Jose <jose.benitez@cern.ch>
2022-04-20 08:40:34 -0700

    fix plots

--- PCCAnalysis/test_Run3/verifyT0ReplayDBUploads.C
+++ PCCAnalysis/test_Run3/verifyT0ReplayDBUploads.C
@@ -10,13 +10,85 @@ using namespace std;
 #define rmin 0.999999
 #define rmax 1.000001
 
+TString inputPath="./verify-T0Replay-RD";
+
+/////////////////////////////////////////////////////
+void readPayload_csv(TString filePath, int LS, float * SF){
+
+  for(int bx=1;bx<=NBX;bx++)
+    SF[bx-1]=0.;
+
+  ifstream csvfile((filePath+"/rawPCC.csv").Data());
+  if (!csvfile.is_open()){
+    cout << "Unable to open file: "<<filePath.Data()<<endl;
+    return;
+  }
+
+  std::string line;
+  int run=0;
+  int ls=0;
+  double lsL=0; //lumi for given LS
+  double sf=0.;
+  while (std::getline(csvfile, line)){
+    //cout<<line;
+
+    std::stringstream iss(line);
+    std::string token;
+
+    ///read the run number
+    std::getline(iss,token, ',');
+    std::stringstream runiss(token);
+    runiss>>run;
+    
+    ///read the LS
+    std::getline(iss,token, ',');
+    std::stringstream lsiss(token);
+    lsiss>>ls;
+
+    if(ls!=LS) continue;
+
+    cout<<"Found payload at ls="<<ls<<endl;
+      
+    ///read the  Lumi per ls (only due to file format)
+    std::getline(iss,token, ',');
+    std::stringstream totLiss(token);
+    totLiss>>lsL;
+    
+    
+    ///
+    for(int bx=1;bx<=NBX;bx++){
+      std::getline(iss,token, ',');
+      std::stringstream bxLiss(token);
+      bxLiss>>SF[bx-1];
+    }
+
+    break;
+  }    
+  csvfile.close();
+    
+}
+
+/////////////////////////////////////////////////////
+int getLS(std::string name){
+  std::istringstream input(name.c_str());
+  std::string s;
+  int lsidc=0;
+  while (std::getline(input, s, '_')){
+    if(lsidc==3) return  atoi(s.c_str()) + 1 ;
+    lsidc++;
+  }
+  return 0;
+}
+
+
+///////////////////////////////////////////////////////
 void verifyT0ReplayDBUploads(){
 
   gStyle->SetPadLeftMargin(0.15);
-  TCanvas C;
-    
+
+
   ///Input from private test: step2 + step3 + step4
-  TFile RootFile("CorrectionHisto.root","read");
+  TFile RootFile(inputPath+"/CorrectionHisto.root","read"); 
 
   ///Histograms corresponding to payload:
   std::list<std::string> payload;
@@ -33,176 +105,120 @@ void verifyT0ReplayDBUploads(){
   payload.push_back("ScaleFactorsAvg_346512_10_519_570");
   payload.push_back("ScaleFactorsAvg_346512_11_571_622");
   cout<<"Number of payloads: "<<payload.size()<<endl;
-  
-  TH1F HScaleFactorsRatio("HScaleFactorsRatio","",NBX,0.5,NBX+0.5);  
-  TH1F HScaleFactorsRatioProj("HScaleFactorsRatioProj","",200,rmin,rmax);
-  TH2F H2ScaleFactorsRatio("H2ScaleFactorsRatio","",NBX,0.5,NBX+0.5,200,rmin,rmax);  
 
+  
+  TGraph G;//flattened scale factors vs bcid
+  TGraph GDB;//flattened scale factors vs bcid
+  TGraph GRatio;//ratios  
+  TH2F H2ScaleFactorsRatio("H2ScaleFactorsRatio","",NBX,0.5,NBX+0.5,100,rmin,rmax);  //ratio values vs bx  in zoom
+
+  TString selectPayload="ScaleFactorsAvg_346512_11_571_622";
+  TGraph GPL;//flattened scale factors vs bcid
+  TGraph GPLDB;//flattened scale factors vs bcid
+  TGraph GPLRatio;// ratios
+
+  float SF[NBX];
+  
+  int plcounter=0;
   std::list<std::string>::iterator it;
   for (it = payload.begin(); it != payload.end(); ++it){
-    TString PayloadName=it->c_str();
+    plcounter++;
     
-    //if(PayloadName.CompareTo("ScaleFactorsAvg_346512_9_467_518") !=0 )   continue;
-    //if(PayloadName.CompareTo("ScaleFactorsAvg_346512_11_571_622") !=0 )   continue;
-        
     std::cout << it->c_str()<<std::endl;
-    
-    std::istringstream input(it->c_str());
-    std::string s;
-    long lsid = 0;
-    int lsidc=0;
-    while (std::getline(input, s, '_')){
-      if(lsidc==3) lsid = atoi(s.c_str()) + 1 ;
-      lsidc++;
-    }
+    long lsid = getLS(it->c_str());
     cout << lsid << endl;
-    
     
     TH1F* HScaleFactors=(TH1F*)RootFile.Get(it->c_str());
     if(!HScaleFactors) return;
-    //cout<<"Nbins : "<<HScaleFactors->GetNbinsX() <<" / "<<NBX<<endl;
-    TH1F HScaleFactorsDB("HScaleFactorsDB","",NBX,0.5,NBX+0.5);
 
-    
-    /////////////////////////////////////////////////////
-    ///Scale factors produced by reading the CONDDB 
-    TString csvfilename="./rawPCC.csv";
-    ifstream csvfile(csvfilename.Data());
-    if (!csvfile.is_open()){
-      cout << "Unable to open file: "<<csvfilename.Data()<<endl;
-      return;
-    }
 
-    std::string line;
-    int run=0;
-    int ls=0;
-    double lsL=0; //lumi for given LS
-    while (std::getline(csvfile, line)){
-      //cout<<line;
+    readPayload_csv(inputPath,lsid,SF);
 
-      std::stringstream iss(line);
-      std::string token;
-
-      ///read the run number
-      std::getline(iss,token, ',');
-      std::stringstream runiss(token);
-      runiss>>run;
-    
-      ///read the LS
-      std::getline(iss,token, ',');
-      std::stringstream lsiss(token);
-      lsiss>>ls;
-
-      //cout<<run<<"  "<<ls<<endl;
-      if(ls!=lsid) continue;
-      cout<<"Found payload at ls="<<ls<<endl;
-      
-      ///read the  Lumi per ls (only due to file format)
-      std::getline(iss,token, ',');
-      std::stringstream totLiss(token);
-      totLiss>>lsL;
-    
-    
-      ///fill lumi per BX plots
-      for(int bx=1;bx<=NBX;bx++){
-	double privateValue=HScaleFactors->GetBinContent(bx-1);
-  
-	double conddbValue=0.;
-	
-	std::getline(iss,token, ',');
-	std::stringstream bxLiss(token);
-	bxLiss>>conddbValue;
-	HScaleFactorsDB.SetBinContent(bx,conddbValue);
-
-	if(conddbValue!=0.){
-	  float r=privateValue/conddbValue;
-	  
-	  if(r<rmin || r>rmax){
-	    cout<<"bad ratio: bx = "<<bx<<" , TEST = "<<privateValue<<" , CONDDB = "<<conddbValue<<endl;
-	  }
-	  
-	  H2ScaleFactorsRatio.Fill(bx,privateValue/conddbValue);
-	  if(PayloadName.CompareTo("ScaleFactorsAvg_346512_9_467_518") ==0 ){	    
-	    HScaleFactorsRatio.Fill(bx,privateValue/conddbValue);
-	    HScaleFactorsRatioProj.Fill(privateValue/conddbValue);
-	  }
-	}
-	else if(conddbValue==0. && privateValue==0.){
-	  H2ScaleFactorsRatio.Fill(bx,1);
-	  if(PayloadName.CompareTo("ScaleFactorsAvg_346512_9_467_518") ==0 ){	    
-	    HScaleFactorsRatio.SetBinContent(bx,1);
-	    HScaleFactorsRatioProj.Fill(1);
-	  }
-	}
-	else{
-	  cout<<"bad ratio: bx = "<<bx<<" , TEST = "<<privateValue<<" , CONDDB = "<<conddbValue<<endl;
-	  H2ScaleFactorsRatio.Fill(bx,0);
-	  if(PayloadName.CompareTo("ScaleFactorsAvg_346512_9_467_518") ==0 ){	    
-	    HScaleFactorsRatio.SetBinContent(bx,0);
-	    HScaleFactorsRatioProj.Fill(0);
-	  }
-	}
+    for(int bx=1;bx<=NBX;bx++){
+      double privateValue = HScaleFactors->GetBinContent(bx-1);
+      double conddbValue  = SF[bx-1];
+ 
+      ///Make ratio
+      float r=0.;
+      if(conddbValue!=0.)
+	r=privateValue/conddbValue;
+      else if(conddbValue==0. && privateValue==0.)
+	r=1.;
+      else
+	r=0.;
+ 	
+      if(r<rmin || r>rmax){
+	cout<<"bad ratio: bx = "<<bx<<" , TEST = "<<privateValue<<" , CONDDB = "<<conddbValue<<endl;
       }
 
-      break;
-    }    
-    csvfile.close();
 
-    if(PayloadName.CompareTo("ScaleFactorsAvg_346512_9_467_518") ==0 ){
-
-      HScaleFactors->SetTitle(PayloadName+" private test");
-      HScaleFactors->SetMarkerColor(1);
-      HScaleFactors->GetYaxis()->SetRangeUser(0.98,1.02);
-      HScaleFactors->SetStats(0);
-      HScaleFactors->GetXaxis()->SetTitle("bcid");
-      C.Clear();
-      HScaleFactors->Draw("histp");
-      C.Print("verifyT0ReplayDBUploads.png");
-
-      HScaleFactorsDB.SetTitle(PayloadName+" CONDDB");
-      HScaleFactorsDB.SetMarkerColor(1);
-      HScaleFactorsDB.GetYaxis()->SetRangeUser(0.98,1.02);
-      HScaleFactorsDB.SetStats(0);
-      HScaleFactorsDB.GetXaxis()->SetTitle("bcid");
-      C.Clear();
-      HScaleFactorsDB.Draw("histp");
-      C.Print("verifyT0ReplayDBUploads_DB.png");
-
+      G.SetPoint(G.GetN(),G.GetN(),privateValue);
+      GDB.SetPoint(GDB.GetN(),GDB.GetN(),conddbValue);
+      GRatio.SetPoint(GRatio.GetN(),GRatio.GetN(),r);
+      H2ScaleFactorsRatio.Fill(bx,r);
+      
+      if(selectPayload.CompareTo(it->c_str()) ==0 ){
+	GPL.SetPoint(GPL.GetN(),bx,privateValue);
+	GPLDB.SetPoint(GPLDB.GetN(),bx,privateValue);
+	GPLRatio.SetPoint(GPLRatio.GetN(),bx,r);
+      }
+      
     }
     
+   
   }
 
 
   
-  HScaleFactorsRatio.SetStats(0);
-  HScaleFactorsRatio.GetYaxis()->SetTitle("ratio");
-  HScaleFactorsRatio.GetYaxis()->SetRangeUser(rmin,rmax);
-  HScaleFactorsRatio.GetXaxis()->SetTitle("bcid");
-  C.Clear();
-  HScaleFactorsRatio.Draw("histp");
-  C.Print("verifyT0ReplayDBUploads_ratio.png");
 
-  gStyle->SetOptStat(111111);
-  HScaleFactorsRatioProj.GetXaxis()->SetTitle(" ratio ");
-  C.Clear();
-  HScaleFactorsRatioProj.Draw("hist");
-  C.Print("verifyT0ReplayDBUploads_proj.png");
+  TCanvas C;
 
-
-  ///all payloads
-  H2ScaleFactorsRatio.SetStats(1);
-  H2ScaleFactorsRatio.GetYaxis()->SetTitle("ratio");
-  H2ScaleFactorsRatio.GetXaxis()->SetTitle("bcid");
+  ///this is for one payload
+  GPL.SetTitle(selectPayload);
+  GPL.GetXaxis()->SetTitle("bcid");
+  GPL.GetYaxis()->SetTitle("Scale factor");
+  GPL.GetYaxis()->SetRangeUser(-0.1,1.1);
   C.Clear();
-  H2ScaleFactorsRatio.Draw("colz");
-  C.Print("verifyT0ReplayDBUploads_ratio2D.png");
+  GPL.Draw("al");
+  GPLDB.SetMarkerColor(2);
+  GPLDB.Draw("lsame"); 
+  C.Print(inputPath+"/"+"verifyT0ReplayDBUploads_valuesPL.png");
 
-  TH1F*Proj2D=(TH1F*)H2ScaleFactorsRatio.ProjectionY("Proj2D");
+  GPLRatio.SetTitle(selectPayload);
+  GPLRatio.GetYaxis()->SetTitle("Scale factor ratio");
+  GPLRatio.GetXaxis()->SetTitle("LS * bcid");
+  GPLRatio.GetYaxis()->SetRangeUser(-0.1,2);
   C.Clear();
-  Proj2D->Draw("hist");
-  C.Print("verifyT0ReplayDBUploads_proj2D.png");
+  GPLRatio.Draw("al"); 
+  C.Print(inputPath+"/"+"verifyT0ReplayDBUploads_valuesPL_Ratio.png");
 
   
+  ////all payloads Flattenned
+  G.SetTitle("all payloads");
+  G.GetXaxis()->SetTitle("LS * bcid");
+  G.GetYaxis()->SetTitle("Scale factor");
+  G.GetYaxis()->SetRangeUser(-0.1,1.1);
+  C.Clear();
+  G.Draw("al");
+  GDB.SetMarkerColor(2);
+  GDB.Draw("lsame"); 
+  C.Print(inputPath+"/"+"verifyT0ReplayDBUploads_values.png");
+
+  GRatio.SetTitle("all payloads");
+  GRatio.GetYaxis()->SetTitle("Scale factor ratio");
+  GRatio.GetXaxis()->SetTitle("LS * bcid");
+  GRatio.GetYaxis()->SetRangeUser(-0.1,2);
+  C.Clear();
+  GRatio.Draw("al"); 
+  C.Print(inputPath+"/"+"verifyT0ReplayDBUploads_values_Ratio.png");
+
+  gStyle->SetOptStat(111111);
+  H2ScaleFactorsRatio.SetStats(1);
+  H2ScaleFactorsRatio.SetTitle("all payloads");
+  C.Clear();
+  H2ScaleFactorsRatio.Draw("histcolz"); 
+  C.Print(inputPath+"/"+"verifyT0ReplayDBUploads_values_RatioZoom.png");
+
   
 }
 
