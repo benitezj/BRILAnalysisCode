c2cb6fd7f3ccad7fec0df359046313495a1a76f6
ashish <asehrawat1991@gmail.com>
2022-04-21 17:23:01 +0200

    csv files old new veto

--- PCCAnalysis/plots/compareZBfiles_oldnewveto.C
+++ PCCAnalysis/plots/compareZBfiles_oldnewveto.C
@@ -6,6 +6,7 @@ void compareZBfiles_oldnewveto() {
   
   TCanvas*C = new TCanvas("Luminosity ratio (new old veto)","");
   C->cd();
+
   TString Path1="/eos/user/a/asehrawa/BRIL-new/";
   
   std::vector<int> run_number = {315252, 315255, 315257, 315258, 315259, 315264, 315265, 315267, 315270, 315322, 315339, 315357, 315361, 315363, 315365, 315366, 315420, 315488, 315489, 315490, 315506, 315509, 315510, 315512, 315533, 315543, 315555, 315556, 315557, 315587, 315640, 315641, 315642, 315644, 315645, 315646, 315647, 315648, 315689, 315690, 315702, 315703, 315704, 315705, 315713, 315721, 315741, 315764, 315770, 315784, 315785, 315786, 315787, 315788, 315789, 315790, 315800, 315801, 315840, 315973, 315974, 316058, 316059, 316060, 316061, 316062, 316082, 316095, 316109, 316110, 316111, 316112, 316113, 316114, 316151, 316153, 316186, 316187, 316199, 316200, 316201, 316202, 316216, 316217, 316218, 316219, 316239, 316240, 316241, 316260, 316271, 316361, 316362, 316363, 316377, 316378, 316379, 316380, 316455, 316456, 316457, 316469, 316470, 316472, 316477, 316478, 316505, 316569, 316590, 316613, 316615, 316664, 316665, 316666, 316667, 316700, 316701, 316702, 316715, 316716, 316717, 316718, 316719, 316720, 316721, 316722, 316723, 316758, 316766, 316797, 316798, 316799, 316808, 316820, 316839, 316851, 316876,316877, 316878, 316879, 316928, 316944, 316985, 316993, 316994, 316995};
@@ -28,8 +29,7 @@ void compareZBfiles_oldnewveto() {
   int LS1=0;
   int previousrunlumisec_count1=0;
   
-  std::map<int, float> cluster_count;
-  
+  std::map<int, float> cluster_count;  
   for (unsigned int j=0;j<run_number.size();j++){
     TString Path = "/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018A";
     TString infile=Path+"/"+run_number.at(j)+".csv"; 
@@ -126,13 +126,10 @@ void compareZBfiles_oldnewveto() {
     
   }
   
-  
   LumiperLS->Draw("histp");
   C->Print(Path1+"Lumiperls_oldveto"+".png");
-  
   LumiperLS1->Draw("histp");
   C->Print(Path1+"Lumiperls_newveto"+".png");
-  
   LumiLSratio ->SetTitle("Run2018A (315252-316995)");
   //LumiLSratio ->SetTitle("run number 316186");
   LumiLSratio ->GetXaxis()->SetTitle("Lumi section");                                                                                 
