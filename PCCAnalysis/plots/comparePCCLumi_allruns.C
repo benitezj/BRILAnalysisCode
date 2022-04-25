f3346fc4703523fb96705245b8cf9127781b72a5
ashish <asehrawat1991@gmail.com>
2022-04-25 21:58:59 +0200

    PCC per bx per run

--- PCCAnalysis/plots/comparePCCLumi_allruns.C
+++ PCCAnalysis/plots/comparePCCLumi_allruns.C
@@ -1,12 +1,22 @@
 #include "globals.h"
-void comparePCCLumi_allruns(){
+void comparePCCLumi_allruns(int option=1, char run_period='C'){
   
   TString Path="/eos/user/a/asehrawa/BRIL-new/PCC_reprocess_2018A_newveto/";
   
   //std::vector<int> run_number = {315252, 315255, 315257, 315258, 315259, 315264, 315265, 315267, 315270, 315322, 315339, 315357, 315361, 315363, 315365, 315366, 315420, 315488, 315489, 315490, 315506, 315509, 315510, 315512, 315533, 315543, 315555, 315556, 315557, 315587, 315640,315641, 315642, 315644, 315645, 315646, 315647, 315648, 315689, 315690, 315702, 315703, 315704, 315705, 315713, 315721, 315741, 315764, 315770, 315784, 315785, 315786, 315787, 315788, 315789, 315790, 315800, 315801, 315840, 315973, 315974, 316058, 316059, 316060, 316061, 316062, 316082, 316095, 316109, 316110, 316111, 316112, 316113, 316114, 316151, 316153, 316186, 316187, 316199, 316200, 316201, 316202, 316216, 316217, 316218, 316219, 316239, 316240, 316241, 316260, 316271, 316361, 316362, 316363, 316377, 316378, 316379, 316380, 316455, 316456, 316457, 316469, 316470, 316472, 316477, 316478, 316505, 316569, 316590, 316613, 316615, 316664, 316665, 316666, 316667, 316700, 316701, 316702, 316715, 316716, 316717, 316718, 316719, 316720, 316721, 316722, 316723, 316758,316766, 316797, 316798, 316799, 316808, 316820, 316839, 316851, 316876,316877, 316878, 316879, 316928, 316944, 316985, 316993, 316994, 316995}; 
+  
 
-  std::vector<int> run_number = {315690};
-
+  std::vector<int> run_number;
+  if(run_period=='A'){
+    run_number = {315690};
+  }
+  if(run_period=='B'){
+   run_number = {317511};
+  }      
+  if(run_period=='C'){
+    run_number = {319486};
+  }
+  
   //std::vector<int> run_number = {317080, 317087, 317088, 317089, 317170, 317182, 317212, 317213, 317239, 317279,317291, 317292, 317295, 317296, 317297, 317319, 317320,317338, 317339, 317340, 317382, 317383, 317391, 317392, 317434, 317435, 317438, 317475, 317478, 317479, 317480, 317481, 317482, 317484, 317488, 317509, 317510, 317511, 317512, 317527, 317580, 317591, 317626, 317640, 317641, 317648,317649, 317650, 317661, 317663, 317683, 317696, 318070, 318622, 318653, 318661, 318662, 318663, 318667, 318669, 318670, 318675, 318712,318714, 318733, 318734, 318785, 318816, 318817, 318819, 318820, 318828, 318834, 318836, 318837, 318872, 318874, 318876, 318877, 318939, 318944, 318945, 318953, 318980, 318981, 318982, 318983, 318984, 318992, 319006, 319011, 319015, 319016, 319018, 319019, 319077, 319097, 319098, 319099, 319100, 319101, 319103, 319104, 319124, 319125, 319159, 319160, 319173, 319174, 319175, 319176, 319177, 319190, 319222, 319223, 319254, 319255, 319256, 319260, 319262, 319263, 319264, 319265, 319266, 319267, 319268, 319270, 319273, 319274, 319300, 319310, 319311};
 
 
@@ -21,10 +31,18 @@ void comparePCCLumi_allruns(){
   
     std::cout<<run_number.at(j)<<std::endl;
   
-    TString Path1="/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018A";
-    //TString Path1="/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018B";
-    //TString Path1="/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018C";
- 
+    TString Path1;
+    if(run_period=='A'){
+    Path1="/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018A";
+    }
+
+    if(run_period=='B'){
+    Path1="/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018B";
+    }
+    if(run_period=='C'){
+    Path1="/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018C";
+    }
+
     TString CSV=Path1+"/"+run_number.at(j)+".csv";
         
     HCSV = new TH1F(TString("HCSV_")+j,"",NBX,0.5,NBX+0.5);
@@ -65,11 +83,17 @@ void comparePCCLumi_allruns(){
     
     csvfile.close();
     
+    TString Path2;
+    if(run_period=='A'){
+    Path2="/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018A";
+    }
+    if(run_period=='B'){
+    Path2="/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018B";
+    }
+    if(run_period=='C'){
+    Path2="/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018C";
+    }
 
-    TString Path2="/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018A";
-    //TString Path2="/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018B";
-    //TString Path2="/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018C";
-  
     TString CSV1=Path2+"/"+run_number.at(j)+".csv";
     
     HCSV1 = new TH1F(TString("HCSV1_")+j,"",NBX,0.5,NBX+0.5);
@@ -124,12 +148,26 @@ void comparePCCLumi_allruns(){
     HCSV->SetMarkerSize(0.5);
     //HCSV->GetYaxis()->SetRangeUser(0,4000000);
     //HCSV->SetTitle("Run2018A (315255 - 316995)");
-    HCSV->SetTitle("run number 315690");
+    if(run_period=='A'){
+    //HCSV->SetTitle("run number 315690");
+    }
+    if(run_period=='B'){
+    HCSV->SetTitle("run number 317511");
+    }
+    
+    if(run_period=='C'){
+      HCSV->SetTitle("run number 319486");
+    }
+
     //HCSV->SetTitle("Run2018B (317080 - 319311)");                                                                                         
     //HCSV->SetTitle("Run2018C (319337 - 320065)"); 
     HCSV->GetXaxis()->SetTitle("bx");
-    //HCSV->GetYaxis()->SetTitle("PCC");
-    HCSV->GetYaxis()->SetTitle("New/Old");
+    if(option==1){
+    HCSV->GetYaxis()->SetTitle("PCC");
+    }
+    if(option==2){
+    HCSV->GetYaxis()->SetTitle("PCCratio");
+    }
     //HCSV->GetYaxis()->SetRangeUser(0, 70000);
     //HCSV->GetYaxis()->SetRangeUser(0.716, 0.73);
     //HCSV->SetTitle("Zero Bias data with afterglow corrections (run number 316110)");
@@ -144,9 +182,10 @@ void comparePCCLumi_allruns(){
     //HCSV1->GetYaxis()->SetTitle("PCC");
     gStyle->SetOptStat(0);    
     
-    HCSV->Divide(HCSV1);
+    if(option==1){
+      //HCSV->Divide(HCSV1);
     HCSV->Draw("p");
-    //HCSV1->Draw("psame");
+    HCSV1->Draw("psame");
     
     auto legend = new TLegend(0.75, 0.75, 0.88, 0.88);    
     legend->AddEntry(HCSV, "New veto", "l");
@@ -156,8 +195,42 @@ void comparePCCLumi_allruns(){
     legend->SetFillColor(0);
     legend->Draw("same");
   
-    C.Print(Path+TString("csv_file_lumi_comparison_RunA_ratio_315690")+".root");
-    
+    if(run_period=='A'){
+      C.Print(Path+TString("csv_file_lumi_comparison_RunB_315690")+".root");
+    }
+
+    if(run_period=='B'){
+    C.Print(Path+TString("csv_file_lumi_comparison_RunB_317511")+".root");
+    }
+
+
+    if(run_period=='C'){
+      C.Print(Path+TString("csv_file_lumi_comparison_RunC_319486")+".root");
+    }
+
+
+
+    }
+
+    if(option==2){
+      HCSV->Divide(HCSV1);
+      HCSV->Draw("p");
+
+      if(run_period=='A'){
+	C.Print(Path+TString("csv_file_lumi_comparison_RunB_ratio_315690")+".root");
+      }
+
+      if(run_period=='B'){
+      C.Print(Path+TString("csv_file_lumi_comparison_RunB_ratio_317511")+".root");
+      }
+
+      if(run_period=='C'){
+	C.Print(Path+TString("csv_file_lumi_comparison_RunC_ratio_319486")+".root");
+      }
+
+
+
+    }
     //gROOT->ProcessLine(".q");
     
 }
