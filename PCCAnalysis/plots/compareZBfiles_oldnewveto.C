a050c58c6df7629738fb643854409c863241e546
ashish <asehrawat1991@gmail.com>
2022-04-26 23:08:17 +0200

    PCC per ls

--- PCCAnalysis/plots/compareZBfiles_oldnewveto.C
+++ PCCAnalysis/plots/compareZBfiles_oldnewveto.C
@@ -12,9 +12,9 @@ void compareZBfiles_oldnewveto(char run_period='A') {
 
   std::vector<int> run_number;
   if(run_period=='A'){
-    //run_number = {315252, 315255, 315257, 315258, 315259, 315264, 315265, 315267, 315270, 315322, 315339, 315357, 315361, 315363, 315365, 315366, 315420, 315488, 315489, 315490, 315506, 315509, 315510, 315512, 315533, 315543, 315555, 315556, 315557, 315587, 315640, 315641, 315642, 315644, 315645, 315646, 315647, 315648, 315689, 315690, 315702, 315703, 315704, 315705, 315713, 315721, 315741, 315764, 315770, 315784, 315785, 315786, 315787, 315788, 315789, 315790, 315800, 315801, 315840, 315973, 315974, 316058, 316059, 316060, 316061, 316062, 316082, 316095, 316109, 316110, 316111, 316112, 316113, 316114, 316151, 316153, 316186, 316187, 316199, 316200, 316201, 316202, 316216, 316217, 316218, 316219, 316239, 316240, 316241, 316260, 316271, 316361, 316362, 316363, 316377, 316378, 316379, 316380, 316455, 316456, 316457, 316469, 316470, 316472, 316477, 316478, 316505, 316569, 316590, 316613, 316615, 316664, 316665, 316666, 316667, 316700, 316701, 316702, 316715, 316716, 316717, 316718, 316719, 316720, 316721, 316722, 316723, 316758, 316766, 316797, 316798, 316799, 316808, 316820, 316839, 316851, 316876,316877, 316878, 316879, 316928, 316944, 316985, 316993, 316994, 316995};
+    run_number = {315252, 315255, 315257, 315258, 315259, 315264, 315265, 315267, 315270, 315322, 315339, 315357, 315361, 315363, 315365, 315366, 315420, 315488, 315489, 315490, 315506, 315509, 315510, 315512, 315533, 315543, 315555, 315556, 315557, 315587, 315640, 315641, 315642, 315644, 315645, 315646, 315647, 315648, 315689, 315690, 315702, 315703, 315704, 315705, 315713, 315721, 315741, 315764, 315770, 315784, 315785, 315786, 315787, 315788, 315789, 315790, 315800, 315801, 315840, 315973, 315974, 316058, 316059, 316060, 316061, 316062, 316082, 316095, 316109, 316110, 316111, 316112, 316113, 316114, 316151, 316153, 316186, 316187, 316199, 316200, 316201, 316202, 316216, 316217, 316218, 316219, 316239, 316240, 316241, 316260, 316271, 316361, 316362, 316363, 316377, 316378, 316379, 316380, 316455, 316456, 316457, 316469, 316470, 316472, 316477, 316478, 316505, 316569, 316590, 316613, 316615, 316664, 316665, 316666, 316667, 316700, 316701, 316702, 316715, 316716, 316717, 316718, 316719, 316720, 316721, 316722, 316723, 316758, 316766, 316797, 316798, 316799, 316808, 316820, 316839, 316851, 316876,316877, 316878, 316879, 316928, 316944, 316985, 316993, 316994, 316995};
 
-    run_number ={316187};
+    //run_number ={316187};
   }
   
   
@@ -66,8 +66,10 @@ void compareZBfiles_oldnewveto(char run_period='A') {
   int previousrunlumisec_count1=0;
   
   std::map<int, float> cluster_count; 
+  std::map<int, float> cluster_count1;
  
-  TString Path, Path3;
+
+  TString Path, Path2;
   for (unsigned int j=0;j<run_number.size();j++){
     if(run_period=='A'){
        Path = "/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018A";
@@ -100,29 +102,29 @@ void compareZBfiles_oldnewveto(char run_period='A') {
     
     
     if(run_period=='A'){
-      Path3 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018A";
+      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018A";
     }
     if(run_period=='B'){
-      Path3 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018B";
+      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018B";
     }
     if(run_period=='C'){
-      Path3 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018C";
+      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018C";
     } 
     if(run_period=='D'){
-      Path3 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D1";
+      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D1";
     }
     if(run_period=='E'){   
-      Path3 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D2";
+      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D2";
     }
     if(run_period=='F'){
-      Path3 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D3";
+      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D3";
     }
     if(run_period=='G'){    
-      Path3 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D4";
+      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D4";
     }
 
 
-    TString infile1=Path3+"/"+run_number.at(j)+".csv";
+    TString infile1=Path2+"/"+run_number.at(j)+".csv";
     //std::cout<< run_number.at(j)<<std::endl;                                                                                               
     ifstream myfile1 (infile1.Data());
     if (!myfile1.is_open()){
@@ -158,21 +160,14 @@ void compareZBfiles_oldnewveto(char run_period='A') {
       
       LumiperLS->Fill(LS, LumiLS);
       //std::cout<<"old veto "<<run<< "  "<<ls<<"  "<<LumiLS<<std::endl;
-      
       //std::cout<<"old "<<run<<"    "<< lumisec_count<<std::endl;
-
-    }
+      }
     
     previousrunlumisec_count+=lumisec_count; 
     myfile.close();     
     
-    for(std::map<int, float>::iterator it = cluster_count.begin(); it != cluster_count.end(); ++it) {
-      std::cout << "Key: " << it->first << "  Value: " << it->second << std::endl;
-    }
-    
-
     std::string line1;  
-    int run1=0;
+    //int run1=0;
     int ls1=0;
     float LumiLS1=0;
     int lumisec_count1=0;
@@ -182,11 +177,11 @@ void compareZBfiles_oldnewveto(char run_period='A') {
       
       std::getline(iss1,token1, ',');
       std::stringstream runiss1(token1);
-      runiss1>>run1;
+      runiss1>>run;
       
       std::getline(iss1,token1, ',');
       std::stringstream lsiss1(token1);
-      lsiss1>>ls1;
+      lsiss1>>ls;
       
       lumisec_count1++;
       LS1 = previousrunlumisec_count1 + ls1;
@@ -194,16 +189,22 @@ void compareZBfiles_oldnewveto(char run_period='A') {
       std::getline(iss1,token1, ',');
       std::stringstream lumiiss1(token1);
       lumiiss1>>LumiLS1;
+      cluster_count1[ls1]=LumiLS1;
       
       LumiperLS1->Fill(LS1, LumiLS1);
       //std::cout<<"newveto "<<run1<< "  "<<ls<<"  "<<LumiLS1<<std::endl;
-      
-      //if((run==run1) && (ls==ls1)){
-      if((run==run1) && (lumisec_count==lumisec_count1)){
+    
+    }
+
+    previousrunlumisec_count1+=lumisec_count1;    
+    myfile1.close();
+  
+    //if(ls==ls1){
+      //if((run==run1) && (lumisec_count==lumisec_count1)){
 	if (run_period=='A' && LumiLS>=2200000 && LumiLS1>=1900000){
-	  if(LumiLS!=0){
-	    LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
-            //std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;                                                                  
+	  if(cluster_count[ls]!=0){
+	    LumiLSratio->SetPoint(LumiLSratio->GetN(), LS1, cluster_count1[ls1]/cluster_count[ls]);
+            std::cout<< "ratio "<<LS1<<"  "<< cluster_count1[ls1]/cluster_count[ls] <<std::endl;                                             
 	  }
 	}
 	if (run_period=='B' && LumiLS>=3000000 && LumiLS1>=4000000){
@@ -242,18 +243,13 @@ void compareZBfiles_oldnewveto(char run_period='A') {
 	    //std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;
 	  }
 	}
-      }
-      
+	//}
+    
       //std::cout<<"new "<<run<<"    "<< lumisec_count1<<std::endl;
-    }
-    
-    
-    previousrunlumisec_count1+=lumisec_count1;    
-    myfile1.close();
-    
-    
   }
-  
+    
+    
+
   LumiperLS->GetXaxis()->SetTitle("Lumi section");
   LumiperLS->GetYaxis()->SetTitle("PCC");  
   LumiperLS->Draw("histp");
