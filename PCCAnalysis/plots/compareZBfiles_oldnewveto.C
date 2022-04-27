4c3721562b00051e178b2dcf76a1e79b56282b74
ashish <asehrawat1991@gmail.com>
2022-04-28 01:13:52 +0200

    PCC per ls (fixed!)

--- PCCAnalysis/plots/compareZBfiles_oldnewveto.C
+++ PCCAnalysis/plots/compareZBfiles_oldnewveto.C
@@ -9,7 +9,7 @@ void compareZBfiles_oldnewveto(char run_period='B') {
 
   TString Path1="/eos/user/a/asehrawa/BRIL-new/";
   TString Path, Path2; 
-
+  
   std::vector<int> run_number;
   if(run_period=='A'){
     run_number = {315252, 315255, 315257, 315258, 315259, 315264, 315265, 315267, 315270, 315322, 315339, 315357, 315361, 315363, 315365, 315366, 315420, 315488, 315489, 315490, 315506, 315509, 315510, 315512, 315533, 315543, 315555, 315556, 315557, 315587, 315640, 315641, 315642, 315644, 315645, 315646, 315647, 315648, 315689, 315690, 315702, 315703, 315704, 315705, 315713, 315721, 315741, 315764, 315770, 315784, 315785, 315786, 315787, 315788, 315789, 315790, 315800, 315801, 315840, 315973, 315974, 316058, 316059, 316060, 316061, 316062, 316082, 316095, 316109, 316110, 316111, 316112, 316113, 316114, 316151, 316153, 316186, 316187, 316199, 316200, 316201, 316202, 316216, 316217, 316218, 316219, 316239, 316240, 316241, 316260, 316271, 316361, 316362, 316363, 316377, 316378, 316379, 316380, 316455, 316456, 316457, 316469, 316470, 316472, 316477, 316478, 316505, 316569, 316590, 316613, 316615, 316664, 316665, 316666, 316667, 316700, 316701, 316702, 316715, 316716, 316717, 316718, 316719, 316720, 316721, 316722, 316723, 316758, 316766, 316797, 316798, 316799, 316808, 316820, 316839, 316851, 316876,316877, 316878, 316879, 316928, 316944, 316985, 316993, 316994, 316995};
@@ -22,7 +22,7 @@ void compareZBfiles_oldnewveto(char run_period='B') {
     run_number = {317080, 317087, 317088, 317089, 317170, 317182, 317212, 317213, 317239, 317279,317291, 317292, 317295, 317296, 317297, 317319, 317320,317338, 317339, 317340, 317382, 317383, 317391, 317392, 317434, 317435, 317438, 317475, 317478, 317479, 317480, 317481, 317482, 317484, 317488, 317509, 317510, 317511, 317512, 317527, 317580, 317591, 317626, 317640, 317641, 317648,317649, 317650, 317661, 317663, 317683, 317696, 318070, 318622, 318653, 318661, 318662, 318663, 318667, 318669, 318670, 318675, 318712,318714, 318733, 318734, 318785,318816, 318817, 318819, 318820, 318828, 318834, 318836, 318837, 318872, 318874, 318876, 318877, 318939, 318944, 318945, 318953, 318980, 318981, 318982, 318983, 318984, 318992, 319006, 319011, 319015, 319016, 319018, 319019, 319077, 319097, 319098, 319099, 319100, 319101, 319103, 319104, 319124, 319125, 319159, 319160, 319173, 319174, 319175, 319176, 319177, 319190, 319222, 319223, 319254, 319255, 319256, 319260, 319262, 319263, 319264, 319265, 319266, 319267, 319268, 319270, 319273, 319274, 319300, 319310, 319311};
     
   }
-    
+  
   if(run_period=='C'){
     run_number = {319337, 319347, 319348, 319349, 319449, 319450, 319452, 319456, 319459, 319460, 319462, 319463, 319464, 319466, 319467, 319468, 319469, 319470, 319471, 319472, 319486, 319488, 319503, 319506, 319524, 319525, 319526, 319528, 319579, 319606, 319625, 319639, 319656, 319657, 319658, 319659, 319678, 319687, 319697, 319698, 319756, 319840, 319841, 319847, 319848, 319849, 319851, 319852, 319853, 319854, 319908, 319909, 319910, 319912, 319913, 319914, 319915, 319928, 319941, 319942, 319950, 319991, 319992, 319993, 320002, 320003, 320006, 320007, 320008, 320009, 320010, 320011, 320012, 320023, 320024, 320025, 320026, 320038, 320039, 320040, 320058, 320059, 320060, 320061, 320062, 320063, 320064, 320065};
   }
@@ -65,55 +65,55 @@ void compareZBfiles_oldnewveto(char run_period='B') {
   int previousrunlumisec_count1=0;
   int lumisec_count=0;
   int lumisec_count1=0;
-
+  
   std::map<int, float> cluster_count; 
-
-    if(run_period=='A'){
-      Path = "/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018A";
-    }  
-    if(run_period=='B'){
-      Path = "/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018B";
-    }    
-    if(run_period=='C'){
-      Path = "/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018C";
-    }
-    if(run_period=='D' || run_period=='E'){
-      Path = "/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018D";
-    }  
-    if(run_period=='F'){
-      Path ="/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018D_mergeLateRunD/";
-    }
-        
-    if(run_period=='G'){
-      Path ="/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_Dec21_LateRunD/Run2018D";
-    }
- 
-
- if(run_period=='A'){
-      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018A";
-    }
-    if(run_period=='B'){
-      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018B";
-    }
-    if(run_period=='C'){
-      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018C";
-    } 
-    if(run_period=='D'){
-      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D1";
-    }
-    if(run_period=='E'){   
-      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D2";
-    }
-    if(run_period=='F'){
-      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D3";
-    }
-    if(run_period=='G'){    
-      Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D4";
-    }
-
-
+  
+  if(run_period=='A'){
+    Path = "/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018A";
+  }  
+  if(run_period=='B'){
+    Path = "/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018B";
+  }    
+  if(run_period=='C'){
+    Path = "/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018C";
+  }
+  if(run_period=='D' || run_period=='E'){
+    Path = "/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018D";
+  }  
+  if(run_period=='F'){
+    Path ="/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_21Sep2018VdmVeto/Run2018D_mergeLateRunD/";
+  }
+  
+  if(run_period=='G'){
+    Path ="/eos/user/b/benitezj/BRIL/PCC/ZeroBias/AlCaLumiPixels_Dec21_LateRunD/Run2018D";
+  }
+  
+  
+  if(run_period=='A'){
+    Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018A";
+  }
+  if(run_period=='B'){
+    Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018B";
+  }
+  if(run_period=='C'){
+    Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018C";
+  } 
+  if(run_period=='D'){
+    Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D1";
+  }
+  if(run_period=='E'){   
+    Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D2";
+  }
+  if(run_period=='F'){
+    Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D3";
+  }
+  if(run_period=='G'){    
+    Path2 = "/eos/user/a/asehrawa/PCC/EXPRESS_datasets/ZeroBias/Run2018_ZB_test/Run2018D4";
+  }
+  
+  
   for (unsigned int j=0;j<run_number.size();j++){
-   
+    
     TString infile=Path+"/"+run_number.at(j)+".csv"; 
     //std::cout<< run_number.at(j)<<std::endl;  
     
@@ -156,13 +156,13 @@ void compareZBfiles_oldnewveto(char run_period='B') {
     
     //previousrunlumisec_count+=lumisec_count; 
     myfile.close();     
-
+    
     //checking map content
     //for(std::map<int,float>::iterator it = cluster_count.begin(); it != cluster_count.end(); ++it) {
     //std::cout <<run<< " Key: " << it->first << " Value: " << it->second << std::endl;
     //}  
     
-
+    
     TString infile1=Path2+"/"+run_number.at(j)+".csv";
     //std::cout<< run_number.at(j)<<std::endl;                                                                                               
     ifstream myfile1 (infile1.Data());
@@ -170,7 +170,7 @@ void compareZBfiles_oldnewveto(char run_period='B') {
       cout << "Unable to open new file: "<<infile1.Data()<<endl;
       return;
     }
-
+    
     std::string line1;  
     int ls1=0;
     float LumiLS1=0;
@@ -196,66 +196,71 @@ void compareZBfiles_oldnewveto(char run_period='B') {
       
       LumiperLS1->Fill(lumisec_count1, LumiLS1);
       //std::cout<<"newveto "<<run<< "  "<<ls<<"  "<<LumiLS1<<std::endl;
-    
+      
       if (run_period=='A' && LumiLS>=2200000 && LumiLS1>=1900000){
 	if(cluster_count[ls]!=0){
 	  LumiLSratio->SetPoint(LumiLSratio->GetN(), lumisec_count1, LumiLS1/cluster_count[ls1]);
 	  std::cout<<lumisec_count1<<"  "<< LumiLS1<<" "<<cluster_count[ls1]<<"  "<<LumiLS1/cluster_count[ls1] <<std::endl;
 	}
       }
+      
+      
+      if (run_period=='B' && LumiLS>=3000000 && LumiLS1>=4000000){
+	if(LumiLS!=0){
+	  LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
+	  //std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;                                                                  
+	}
+      }
 
+      if (run_period=='C' && LumiLS>=1000000 && LumiLS1>=1000000){
+	if(LumiLS!=0){
+	  LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
+	  //std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;                                                                  
+	}
+      }
+      
+      if (run_period=='D' && LumiLS>=2000000 && LumiLS1>=2000000){
+	if(LumiLS!=0){
+	  LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
+	  //std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;                                                                  
+	}
+      }
 
- if (run_period=='B' && LumiLS>=3000000 && LumiLS1>=4000000){
-      if(LumiLS!=0){
-	LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
-	//std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;                                                                  
+      if (run_period=='E' && LumiLS>=3000000 && LumiLS1>=2000000){
+	if(LumiLS!=0){
+	  LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
+	  //std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;                                                                  
+	}
       }
-    }
-    if (run_period=='C' && LumiLS>=1000000 && LumiLS1>=1000000){
-      if(LumiLS!=0){
-	LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
-	//std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;                                                                  
-      }
-    }
-    if (run_period=='D' && LumiLS>=2000000 && LumiLS1>=2000000){
-      if(LumiLS!=0){
-	LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
-	//std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;                                                                  
-      }
-    }
-    if (run_period=='E' && LumiLS>=3000000 && LumiLS1>=2000000){
-      if(LumiLS!=0){
-	LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
-	//std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;                                                                  
-      }
-    }
-    if (run_period=='F' &&  LumiLS>=2000000 && LumiLS1>=2000000){
-      if(LumiLS!=0){
-	LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
-	//std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;                                                                  
-      }
-    }
-    if (run_period=='G' &&  LumiLS>=1000000 && LumiLS1>=3000000){ 
-      if(LumiLS!=0){
-	LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
-	//std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;
-      }
-    }
 
-    }
+      if (run_period=='F' &&  LumiLS>=2000000 && LumiLS1>=2000000){
+	if(LumiLS!=0){
+	  LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
+	  //std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;                                                                  
+	}
+      }
 
+      if (run_period=='G' &&  LumiLS>=1000000 && LumiLS1>=3000000){ 
+	if(LumiLS!=0){
+	  LumiLSratio->SetPoint(LumiLSratio->GetN(), LS, LumiLS1/LumiLS);
+	  //std::cout<< "ratio "<<LS1<<"  "<< LumiLS1/LumiLS <<std::endl;
+	}
+      }
+      
+    }
+    
     //previousrunlumisec_count1+=lumisec_count1;    
     myfile1.close();
-  
-    for(std::map<int,float>::iterator it = cluster_count.begin(); it != cluster_count.end(); ++it) {
+    
+    //for(std::map<int,float>::iterator it = cluster_count.begin(); it != cluster_count.end(); ++it) {
       //std::cout <<run<< " Key: " << it->first << " Value: " << it->second << std::endl;
-    }
-
+    //}
+    
     
     //std::cout<<"new "<<run<<"    "<< lumisec_count1<<std::endl;
   }
-    
-    
+  
+  
   LumiperLS->GetXaxis()->SetTitle("Lumi section");
   LumiperLS->GetYaxis()->SetTitle("PCC");  
   LumiperLS->Draw("histp");
