05d8fa55f2872f7388f105ba33d1723808786c94
ashish <asehrawat1991@gmail.com>
2022-04-28 20:01:09 +0200

    afterglow plots fixed

--- PCCAnalysis/plots/PCC_scalefactors.C
+++ PCCAnalysis/plots/PCC_scalefactors.C
@@ -1,109 +1,102 @@
-void PCC_scalefactors() {
+void PCC_scalefactors(int option=1) {
   
   TFile*f = new TFile("/eos/user/a/asehrawa/PCC/EXPRESS_datasets/AlCaPCCRandom/Run2018_RD_test/Run2018A/315690.root");
-  //TFile*f = new TFile("/eos/user/a/asehrawa/BRIL-new/PCC_reprocess_2018A_newveto_RD/AlCaPCCRandom/Run2018A/316110.root");
   TFile*f1 = new TFile("/eos/cms/store/cmst3/user/benitezj/BRIL/PCC/AlCaPCCRandom/315690.root");
+
+  //TFile*f = new TFile("/eos/user/a/asehrawa/BRIL-new/PCC_reprocess_2018A_newveto_RD/AlCaPCCRandom/Run2018A/316110.root");
+  //TFile*f1 = new TFile("/eos/cms/store/cmst3/user/benitezj/BRIL/PCC/AlCaPCCRandom/316110.root");
   
   //TFile*f = new TFile("/eos/cms/store/cmst3/user/benitezj/BRIL/PCC/AlCaPCCRandom/317696.root");                                            
-  //TFile*f1 = new TFile("/eos/user/a/asehrawa/BRIL-new/PCC_reprocess_2018B_newveto/AlCaPCCRandom/Run2018B_RD/317696.root");                   
+  //TFile*f = new TFile("/eos/user/a/asehrawa/BRIL-new/PCC_reprocess_2018B_newveto/AlCaPCCRandom/Run2018B_RD/317696.root");
+  
   //TFile*f = new TFile("/eos/cms/store/cmst3/user/benitezj/BRIL/PCC/AlCaPCCRandom/319639.root");
-  //TFile*f1 = new TFile("/eos/user/a/asehrawa/BRIL-new/PCC_reprocess_2018C_newveto/AlCaPCCRandom/Run2018C_RD/319639.root");  
+  //TFile*f = new TFile("/eos/user/a/asehrawa/BRIL-new/PCC_reprocess_2018C_newveto/AlCaPCCRandom/Run2018C_RD/319639.root");  
   
   TCanvas*C = new TCanvas("PCC","");
   
   TH1F * H1 = NULL;
   TH1F * H2 = NULL;
-  
+
+  TH1F *H_new_colliding_bx;
+  H_new_colliding_bx=new TH1F("h_new_collding_bx", "new veto Raw PCC collding bunches", 3500, 0.0, 3500);
+
+  TH1F *H_old_colliding_bx;
+  H_old_colliding_bx=new TH1F("h_old_colliding_bx", "old veto Raw PCC collding bunches", 3500, 0.0, 3500);
+
+
   H1 = (TH1F*)f->Get("ScaleFactorsAvg_315690_10_513_563;1");
   H1->SetLineColor(1);
   H1->SetMarkerColor(1);
   if (H1==NULL){
     return;
   }
-  
+
   H2 = (TH1F*)f1->Get("ScaleFactorsAvg_315690_10_513_563;1");
   H2->SetLineColor(2);
   H2->SetMarkerColor(2);
   if (H2==NULL){
     return;
   }
-  
-  //H1 = (TH2F*)f->Get("ScaleFactorsAvg_317696_0_1_85;1");
-  //H1->SetLineColor(1);
-  //if (H1==NULL){
-  //return;
-  //}
 
-  //H2 = (TH2F*)f1->Get("ScaleFactorsAvg_317696_0_1_85;1");
-  //H2->SetLineColor(2);
-  //if (H2==NULL){
-  //return;
-  //}
+
+  if(option==1){
+    H_new_colliding_bx->SetTitle("run number 315690 lumi block 10 (513-563)");
+    H_new_colliding_bx->GetXaxis()->SetTitle("bx");
+    H_new_colliding_bx->GetYaxis()->SetTitle("background correction");
+    H_new_colliding_bx->GetYaxis()->SetRangeUser(0.9, 1);   
+
+    int x_value;
+    float bincontent;
+    for(int j = 1; j <= H1->GetNbinsX(); j++) {
+      x_value =  H1->GetBinCenter(j);
+      bincontent = H1->GetBinContent(j); 
+      if (bincontent>=0.85){
+	H_new_colliding_bx->Fill(x_value, bincontent);
+	std::cout<<"new "<<x_value<<" "<<bincontent<<std::endl;
+	}
+    }
   
-  //H1 = (TH2F*)f->Get("ScaleFactorsAvg_319639_0_1_78;1");
-  //H1->SetLineColor(1);
-  //if (H1==NULL){
-  //return;
-  // }
-  
-  //H2 = (TH2F*)f1->Get("ScaleFactorsAvg_319639_0_1_78;1");
-  //H2->SetLineColor(2);
-  //if (H2==NULL){
-  //return;
-  // }
-  
+    int x_value1;
+    float bincontent1;
+    for(int k = 1; k <= H2->GetNbinsX(); k++) {
+      x_value1 =  H2->GetBinCenter(k);
+      bincontent1 = H2->GetBinContent(k);
+      if (bincontent1>=0.85){
+	H_old_colliding_bx->Fill(x_value1, bincontent1);
+	std::cout<<"old "<<x_value1<<" "<<bincontent1<<std::endl;      
+	//std::cout<<x_value<<"  "<<bincontent<<"  "<<x_value1<<" "<<bincontent1<<std::endl;      
+	}
+    }
+
+ 
+    H_new_colliding_bx->SetLineColor(1);
+    H_new_colliding_bx->SetMarkerColor(1);
+
+    H_old_colliding_bx->SetLineColor(2);
+    H_old_colliding_bx->SetMarkerColor(2);
+
+    H_new_colliding_bx->Draw("histp");
+    H_old_colliding_bx->Draw("histpsame");
+
+    auto legend = new TLegend(0.75, 0.75, 0.88, 0.88);
+    legend->AddEntry(H_new_colliding_bx, "New veto", "l");
+    legend->AddEntry(H_old_colliding_bx, "Old veto", "l");
+    legend->SetFillColor(0);
+    legend->SetLineColor(0);
+    legend->SetFillColor(0);
+    legend->Draw("same");
+    C->Print("/eos/user/a/asehrawa/BRIL-new/raw_pcc_old_new_scale_factor.png");
+  }
+
   gStyle->SetOptStat(0);
-  H1->GetXaxis()->SetTitle("bx");
-  //H1->GetYaxis()->SetTitle("background correction");
-  H1->GetYaxis()->SetTitle("New/Old");
-  H1->SetTitle("Scale factors ratio for colliding bunches (run 315690 LS block 563)");
-  //H1->SetTitle("Scale factors (run 315690 LS block 563)"); 
-  //H1->SetTitle("Scale factors (run 317696 LS block 85)");
-  //H1->SetTitle("Scale factors (run 319639 LS block 78)"); 
-  
-  int x_value;
-  float bincontent;
-  for(int j = 1; j <= H1->GetNbinsX(); j++) {
-    x_value =  H1->GetBinCenter(j);
-    bincontent = H1->GetBinContent(j);
-    //std::cout<<"H1 "<<x_value<<"  "<<bincontent<<std::endl;
-    //if(bincontent > 0){
-    //gr ->SetPoint(gr->GetN(), x_value, bincontent);
-    //}
+ 
+  if(option==2){
+    H_new_colliding_bx->GetXaxis()->SetTitle("bx");
+    H_new_colliding_bx->GetYaxis()->SetTitle("new/old");
+    //H_new_colliding_bx->GetYaxis()->SetRangeUser(0.8, 1); 
+    H_new_colliding_bx->Divide(H_old_colliding_bx);
+    H_new_colliding_bx->Draw("histp");
+    C->Print("/eos/user/a/asehrawa/BRIL-new/raw_old_new_scale_factor_ratio.png");
   }
-  
-  
-  int x_value1;
-  float bincontent1;
-  for(int k = 1; k <= H2->GetNbinsX(); k++) {
-    x_value1 =  H2->GetBinCenter(k);
-    bincontent1 = H2->GetBinContent(k);
-    //std::cout<<"H2 "<<x_value1<<"  "<<bincontent1<<std::endl;
-    //if(bincontent > 0){                                                                                                                    
-    //gr ->SetPoint(gr->GetN(), x_value, bincontent);                                                                                        
-    //}                                                                                                                                      
-  }
-  
-  
-  //H1->GetYaxis()->SetRangeUser(0.9, 1);
-  H1->Divide(H2);
-  //if((bincontent > 0.8) && (bincontent1 > 0.8)){
-  //}
-  H1->Draw("p");
-  //H2->Draw("psame");
-  //}
-  
-  auto legend = new TLegend(0.75, 0.75, 0.88, 0.88);
-  //legend->AddEntry(H1, "New (AS)", "l");
-  //legend->AddEntry(H2, "Old (JB)", "l");
-  
-  legend->AddEntry(H1, "New veto", "l");
-  legend->AddEntry(H2, "Old veto", "l");
-  
-  legend->SetFillColor(0);
-  legend->SetLineColor(0);
-  legend->SetFillColor(0);
-  //legend->Draw("same");
-  
-  C->Print("/eos/user/a/asehrawa/BRIL-new/scalefactors_old_new_as_jb_RunA_ratio.root");
 }
+
