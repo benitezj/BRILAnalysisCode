1d131d2eb66624d3d87ea1ca2dfc7f3aff5f061a
ashish <asehrawat1991@gmail.com>
2022-06-17 12:17:06 +0200

    updated

--- PCCAnalysis/plots/afterglow_uncertainty.C
+++ PCCAnalysis/plots/afterglow_uncertainty.C
@@ -24,8 +24,6 @@ void afterglow_uncertainty() {
   
   //run_number = {315252, 315255, 315257, 315258, 315259, 315264, 315265, 315267, 315270, 315322, 315339, 315357, 315361, 315363, 315365, 315366, 315420, 315488, 315489, 315490, 315506, 315509, 315510, 315512, 315533, 315543, 315555, 315556, 315557, 315587, 315640, 315641, 315642, 315644, 315645, 315646, 315647, 315648, 315689, 315690, 315702, 315703, 315704, 315705, 315713, 315721, 315741, 315764, 315770, 315784, 315785, 315786, 315787, 315788, 315789, 315790, 315800, 315801, 315840, 315973, 315974, 316058, 316059, 316060, 316061, 316062, 316082, 316095, 316109, 316110, 316111, 316112, 316113, 316114, 316151, 316153, 316186, 316187, 316199, 316200, 316201, 316202, 316216, 316217, 316218, 316219,316239, 316240, 316241, 316260, 316271, 316361, 316362, 316363, 316377, 316378, 316379, 316380, 316455, 316456, 316457, 316469, 316470, 316472, 316477, 316478, 316505, 316569, 316590, 316613, 316615, 316664, 316665, 316666, 316667, 316700, 316701, 316702, 316715, 316716, 316717, 316718, 316719, 316720, 316721, 316722, 316723, 316758, 316766, 316797, 316798, 316799, 316808, 316820, 316839, 316851, 316876,316877, 316878,316879, 316928, 316944, 316985, 316993, 316994, 316995};
 
-    
-
   TFile* f=new TFile("/eos/user/a/asehrawa/afterglow_allrun_test/afterglow_allrun/afterglow.root");
   
   TH1* Residual_type1[1000];
@@ -107,54 +105,57 @@ void afterglow_uncertainty() {
   legend->SetFillColor(0);
   //legend->Draw("same");
   C->Print(outpath+"PCC_afterglow_type1_type2_residualvstime_Run2018A"+".png");
-
-  system(TString("mkdir -p ")+outpath);
-
-  TCanvas C1("C1","",1200,500);
-
-  int run;
-  //std::vector<int> run;
-  run={318653};
   
-  //iterate over the TFile keys
-  TFile FCORR((inpath+"/"+run+".root").Data(),"read");
-  TIter next(FCORR.GetListOfKeys());
-  TObject* key;
-  while ((key = next())) {
-    TString kname(key->GetName());
-    if(!kname.Contains("RawLumiAvg")) continue;
-    TObjArray * a = kname.Tokenize("_");
-    long r=atoi(((TObjString*)(*a)[1])->GetName());
-    long l=atoi(((TObjString*)(*a)[2])->GetName());
-    long ls1=atoi(((TObjString*)(*a)[3])->GetName());
-    long ls2=atoi(((TObjString*)(*a)[4])->GetName());
+  
+  system(TString("mkdir -p ")+outpath);
+  
+  TCanvas C1("C1","",1200,500);
+  
+  std::vector<int> run;
+  
+  for (unsigned int k=0;k<run_number.size();k++){
     
-    if(r!=run) continue;
-    if(l>0) continue;//only look at the first ls block
-
-    TH1F* HRAW = (TH1F*) FCORR.Get(TString("RawLumiAvg_")+r+"_"+l+"_"+ls1+"_"+ls2);
-    if(!HRAW)      continue;
-    float scale=1./HRAW->GetMaximum();
-    //HRAW->Scale(scale);
+    run={run_number.at(k)};
     
-    TH1F* HCORR = (TH1F*) FCORR.Get(TString("CorrectedLumiAvg_")+r+"_"+l+"_"+ls1+"_"+ls2);
-    if(!HCORR)      continue;
-    //HCORR->Scale(scale);
+    //iterate over the TFile keys
+    TFile FCORR((inpath+"/"+run+".root").Data(),"read");
+    TIter next(FCORR.GetListOfKeys());
+    TObject* key;
+    while ((key = next())) {
+      TString kname(key->GetName());
+      if(!kname.Contains("RawLumiAvg")) continue;
+      TObjArray * a = kname.Tokenize("_");
+      long r=atoi(((TObjString*)(*a)[1])->GetName());
+      long l=atoi(((TObjString*)(*a)[2])->GetName());
+      long ls1=atoi(((TObjString*)(*a)[3])->GetName());
+      long ls2=atoi(((TObjString*)(*a)[4])->GetName());
+      
+      if(r!=run) continue;
+      if(l>0) continue;//only look at the first ls block
+      
+      TH1F* HRAW = (TH1F*) FCORR.Get(TString("RawLumiAvg_")+r+"_"+l+"_"+ls1+"_"+ls2);
+      if(!HRAW)      continue;
+      float scale=1./HRAW->GetMaximum();
+      //HRAW->Scale(scale);
+      
+      TH1F* HCORR = (TH1F*) FCORR.Get(TString("CorrectedLumiAvg_")+r+"_"+l+"_"+ls1+"_"+ls2);
+      if(!HCORR)      continue;
+      //HCORR->Scale(scale);
+      
+      C1.Clear();
+      HRAW->SetTitle(TString("Run: ")+run+" , LS: "+ls1+" - "+ls2);
+      HRAW->GetYaxis()->SetTitle("counts");
+      HRAW->GetXaxis()->SetTitle("bcid");
+      HRAW->GetYaxis()->SetRangeUser(-0.1,1.1*HRAW->GetMaximum());
+      HRAW->SetMarkerColor(1);
+      HRAW->Draw("histp");
+      HCORR->SetMarkerColor(4);
+      //HCORR->Draw("histpsame");
+      C1.Print(outpath+"/compareRandomRawCorr_"+run+"_"+l+".png");
     
-    C1.Clear();
-    HRAW->SetTitle(TString("Run: ")+run+" , LS: "+ls1+" - "+ls2);
-    HRAW->GetYaxis()->SetTitle("counts");
-    HRAW->GetXaxis()->SetTitle("bcid");
-    HRAW->GetYaxis()->SetRangeUser(-0.1,1.1*HRAW->GetMaximum());
-    HRAW->SetMarkerColor(1);
-    HRAW->Draw("histp");
-    HCORR->SetMarkerColor(4);
-    //HCORR->Draw("histpsame");
-    C1.Print(outpath+"/compareRandomRawCorr_"+run+"_"+l+".png");
-    
-    l++;
+      l++;
+    }
   }
-
 
   gROOT->ProcessLine(".q");
 }
