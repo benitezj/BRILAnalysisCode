5f75359a007284970621faae7445f27387afca10
ashish <asehrawat1991@gmail.com>
2022-07-13 05:07:07 +0200

    hfoc json file

--- PCCAnalysis/plots/readjson.C
+++ PCCAnalysis/plots/readjson.C
@@ -1,49 +1,47 @@
-//#include </home/ashish/vcpkg/installed/x64-linux/include/json/json.h>
-#include <fstream>
+#include "/home/ashish/json/single_include/nlohmann/json.hpp"
 #include <iostream>
-#include <string.h>
-#include<stdio.h>
+#include <fstream>
+using json = nlohmann::json;
+using namespace std;
 void readjson(){
   
-  //Json::Value val;
-  //Json::Reader reader;
-  std::string normtag;
-  std::string run;
-  std::string goodlsrangebegin;
-  std::string goodlsrangeend;
-  std::string line;
-  int lineNum;
-  std::ifstream file("/cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_hfoc.json");
-  if (file.is_open()){
-    lineNum = 0;
-    while(std::getline(file, line)) {
-      lineNum++;
-      //reader.parse(file, line)      
-      std::stringstream iss(line);
-      std::string token;
-      
-      std::getline(iss,token, ',');
-      std::stringstream normtagiss(token);
-      normtagiss>>normtag;
-      
-      std::getline(iss,token,':');
-      std::stringstream runiss(token);
-      runiss>>run;
-      
-      std::getline(iss,token,',');
-      std::stringstream lsissbegin(token);
-      lsissbegin>>goodlsrangebegin;
-      
-      std::getline(iss,token);
-      std::stringstream lsissend(token);
-      lsissend>>goodlsrangeend;
-      
-      std::cout<<//normtag<<" "<<
-	run<<"  "<<goodlsrangebegin<<" , "<<goodlsrangeend<<endl;
-      //cout <<line<<endl;
-       
-    }
+  std::ifstream file("/home/ashish/vcpkg/normtag_hfoc.json");
+  json j;
+  file >> j;
+  //std::cout<<j<<endl;
+  
+  for (json::iterator it = j.begin(); it != j.end(); ++it) {
+    //std::cout << *it << '\n';
   }
+  
+  for (auto& elem : j) {
+    //std::cout << "str = " << elem.at(0).get<std::string>() << std::endl;
+    auto &subelem = elem.at(1);
+    //std::cout<<elem.at(1)<<std::endl;
+    for (auto &subsub : subelem) {
+      //std::cout << subsub.key() << " : " << subsub.value() << std::endl;
+    }
+    for (json::iterator it1 = elem.at(1).begin(); it1 != elem.at(1).end(); ++it1) {
+      std::cout << "run number: " << it1.key() << ", good ls range:" << it1.value() << '\n';
+      //std::cout << *it1 << '\n';
+    }
+    
+    //std::cout << element << '\n';
+  }
+  //std::cout<<j.size()<<std::endl;
 }
 
+
+/**......................
+   auto arr = nlohmann::json::parse("[[\"hfoc15PaperV0\", {\"246958\": [[1, 71]]}],"
+   "[\"hfoc16PaperV0\", {\"246959\": [[1, 98]]}]]");
+   
+   for (auto &elem : arr) {
+   std::cout << "str = " << elem.at(0).get<std::string>() << std::endl;
+   auto &subelem = elem.at(1);
+   for (auto &subsub : subelem) {
+   std::cout << subsub.key() << " : " << subsub.value() << std::endl;
+   }
+   }
+   ....................**/
 // path to hfoc json file /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_hfoc.json
