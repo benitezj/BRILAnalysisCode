407e7c086fbaa138813885614cad7b2b2b02bd65
ashish <asehrawat1991@gmail.com>
2022-07-16 20:40:45 +0200

    read json in python

--- PCCAnalysis/plots/readjson.py
+++ PCCAnalysis/plots/readjson.py
@@ -2,7 +2,7 @@ import json
 with open('/cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_hfoc.json', "r") as HFOC_JSON:
     data = json.load(HFOC_JSON)
 
-with open("flat.txt", "w") as HFOC_JSON:
+with open("hfoc_normtag.txt", "w") as HFOC_JSON:
     for line in data:
         if not line[0].startswith("hfoc"):
             continue
