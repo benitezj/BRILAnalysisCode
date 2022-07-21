535e017813bdb726f1bc009d4d42082f67079965
ashish <asehrawat1991@gmail.com>
2022-07-21 03:20:14 +0200

    updated

--- PCCAnalysis/python/plotPCCcsv.py
+++ PCCAnalysis/python/plotPCCcsv.py
@@ -13,7 +13,7 @@ import json
 C1=ROOT.TCanvas("C1","",2000,1000)
 h_ratio = ROOT.TH2F("h_ratio", "PCC/HFOC vs lumi section histogram ", 500, 0.0, 50000, 500, 0, 2)                  
 h_ratiovsHF = ROOT.TH2F("h_ratiovsHF", "PCC/HFOC vs HFOC Histogram", 200, 0.0, 23000, 200, 0.0, 2)
-PCCvsHFOC=ROOT.TH2F("h_PCCvsHFOC", "PCC vs HFOC Histogram", 200, 0.0, 30000, 200, 0.0, 30000)
+PCCvsHFOC=ROOT.TH2F("h_PCCvsHFOC", "PCC vs HFOC Histogram", 200, 0.0, 50000, 200, 0.0, 50000)
 ProfX_h_ratiovsHF_residual=ROOT.TGraph()
 ProfX_PCCvsHF_residual=ROOT.TGraph()
 lumisec_count=0
@@ -28,8 +28,8 @@ with open("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/s
         ls=data.split()[1]
         PCC_count=data.split()[2]
         HFOC_count=data.split()[3]
-        goodls=False
         lumisec_all=lumisec_all+1
+        goodls=False
         for line in json_data:
             if not line[0].startswith("hfoc"):
                 continue
@@ -44,7 +44,7 @@ with open("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/s
                                 lumisec_good=lumisec_good+1
                                 goodls=True
                                 ##print run, run1, ls, ls1, ls2
-        if goodls==True and float(HFOC_count) !=0:
+        if goodls==True and float(HFOC_count) !=0 and float(PCC_count)>=8000 and float(HFOC_count)>=8000:
             h_ratiovsHF.Fill(float(HFOC_count), float(PCC_count)/float(HFOC_count))
             h_ratio.Fill(lumisec_count+1, float(PCC_count)/float(HFOC_count))
             lumisec_count=lumisec_count+1                          
@@ -52,23 +52,27 @@ with open("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/s
             ##print lumisec_count, int(ls), ls1, ls2, float(PCC_count)/float(HFOC_count)
             ##print int(run), int(run1), ls, ls1, ls2 
 
-print lumisec_all, lumisec_good
+print (lumisec_all, lumisec_good)
 ProjY_h_ratio=h_ratio.ProjectionY("Y Projection of PCC/HFOC vs ls", 0, 43989)    
 ProfX_h_ratiovsHF=h_ratiovsHF.ProfileX()
-fitfn = ROOT.TF1("fitfn","[0]*x+[1]",0,25000);
+fitfn = ROOT.TF1("fitfn","[0]*x+[1]",0,50000);
 ProfX_h_ratiovsHF.Fit("fitfn")
 ProfX_PCCvsHFOC=PCCvsHFOC.ProfileX()
-fitfn1 = ROOT.TF1("fitfn1","[0]*x+[1]",0,25000);
+fitfn1 = ROOT.TF1("fitfn1","[0]*x+[1]",0,50000);
 ProfX_PCCvsHFOC.Fit("fitfn1")
 
-##for i in ls:
-##    if fitfn.Eval(ProfX_h_ratiovsHF.GetBinCenter(int(i)))!=0:
-##        ProfX_h_ratiovsHF_residual.SetPoint(ProfX_h_ratiovsHF_residual.GetN(), ProfX_h_ratiovsHF.GetBinCenter(int(i)), ((ProfX_h_ratiovsHF.GetBinContent(int(i)))-fitfn.Eval(ProfX_h_ratiovsHF.GetBinCenter(int(i))))/fitfn.Eval(ProfX_h_ratiovsHF.GetBinCenter(int(i))));
-
-##for i in ls:
-##    if fitfn1.Eval(ProfX_PCCvsHFOC.GetBinCenter(int(i)))!=0:         
-##        ProfX_PCCvsHF_residual.SetPoint(ProfX_PCCvsHF_residual.GetN(), ProfX_PCCvsHFOC.GetBinCenter(int(i)), ((ProfX_PCCvsHFOC.GetBinContent(int(i)))-fitfn1.Eval(ProfX_PCCvsHFOC.GetBinCenter(int(i))))/fitfn1.Eval(ProfX_PCCvsHFOC.GetBinCenter(int(i))));  
-
+## run loop, lumi section loop
+with open("/afs/cern.ch/user/a/asehrawa/Reprocessed_PCC_2018_data/CMSSW_10_2_2/src/PCC_hfoc_plots/EXPRESS_datasets/Run2018_ZB_test/Run2018B/ls.dat", "r") as datFile:
+    for data in datFile:
+        run=data.split()[0]
+        ls=data.split()[1]
+        ##if int(run)==317663:
+        if fitfn.Eval(ProfX_h_ratiovsHF.GetBinCenter(int(ls)))!=0 and ProfX_h_ratiovsHF.GetBinContent(int(ls))!=0:
+            ProfX_h_ratiovsHF_residual.SetPoint(ProfX_h_ratiovsHF_residual.GetN(), ProfX_h_ratiovsHF.GetBinCenter(int(ls)), ((ProfX_h_ratiovsHF.GetBinContent(int(ls)))-fitfn.Eval(ProfX_h_ratiovsHF.GetBinCenter(int(ls))))/fitfn.Eval(ProfX_h_ratiovsHF.GetBinCenter(int(ls))));
+            print int(run), int(ls), ProfX_h_ratiovsHF.GetBinCenter(int(ls)), ProfX_h_ratiovsHF.GetBinCenter(int(ls)), ProfX_h_ratiovsHF.GetBinContent(int(ls)), fitfn.Eval(ProfX_h_ratiovsHF.GetBinCenter(int(ls))), ((ProfX_h_ratiovsHF.GetBinContent(int(ls)))-fitfn.Eval(ProfX_h_ratiovsHF.GetBinCenter(int(ls))))/fitfn.Eval(ProfX_h_ratiovsHF.GetBinCenter(int(ls)))
+            if fitfn1.Eval(ProfX_PCCvsHFOC.GetBinCenter(int(ls)))!=0 and ProfX_PCCvsHFOC.GetBinContent(int(ls))!=0:         
+                ProfX_PCCvsHF_residual.SetPoint(ProfX_PCCvsHF_residual.GetN(), ProfX_PCCvsHFOC.GetBinCenter(int(ls)), ((ProfX_PCCvsHFOC.GetBinContent(int(ls)))-fitfn1.Eval(ProfX_PCCvsHFOC.GetBinCenter(int(ls))))/fitfn1.Eval(ProfX_PCCvsHFOC.GetBinCenter(int(ls))));  
+    
 h_ratiovsHF.SetMarkerStyle(20)
 h_ratiovsHF.SetMarkerColor(46)
 h_ratiovsHF.GetXaxis().SetTitle("HF Inst. Lumi")
@@ -102,14 +106,16 @@ ProfX_PCCvsHFOC.Draw("colz")
 C1.Print('/eos/user/a/asehrawa/BRIL-new/'+'PCCvsHFOC_ProfileX.png')
 ProfX_h_ratiovsHF_residual.SetMarkerStyle(20)
 ProfX_h_ratiovsHF_residual.SetMarkerColor(46)
-ProfX_h_ratiovsHF_residual.GetXaxis().SetTitle("(PCC-Fit)/Fit")
-ProfX_h_ratiovsHF_residual.GetYaxis().SetTitle("Entries")
+ProfX_h_ratiovsHF_residual.GetYaxis().SetTitle("(PCC-Fit)/Fit")
+ProfX_h_ratiovsHF_residual.GetXaxis().SetTitle("HFOC")
+ProfX_h_ratiovsHF_residual.SetTitle("Stability Systematics")
 ProfX_h_ratiovsHF_residual.Draw("AP")
 C1.Print('/eos/user/a/asehrawa/BRIL-new/'+'PCC_HFOCvsHFOC_ProfileX_residuals.png')
 ProfX_PCCvsHF_residual.SetMarkerStyle(20)
 ProfX_PCCvsHF_residual.SetMarkerColor(46)
-ProfX_PCCvsHF_residual.GetXaxis().SetTitle("(PCC-Fit)/Fit")
-ProfX_PCCvsHF_residual.GetYaxis().SetTitle("Entries")
+ProfX_PCCvsHF_residual.GetYaxis().SetTitle("(PCC-Fit)/Fit")
+ProfX_PCCvsHF_residual.GetXaxis().SetTitle("HFOC")
+ProfX_PCCvsHF_residual.SetTitle("Linearity systematics")
 ProfX_PCCvsHF_residual.Draw("AP")
 C1.Print('/eos/user/a/asehrawa/BRIL-new/'+'PCCvsHFOC_ProfileX_residuals.png')
 ProjY_h_ratio.SetMarkerStyle(20)
@@ -123,3 +129,4 @@ C1.Print('/eos/user/a/asehrawa/BRIL-new/'+'PCCvsHFOC_ProjectionY.png')
 
 
 
+## Selection for low lumi ls PCC_count>=3000000 and HFOC_count>=4000000
