a7f2362fb02601a84cf9246c39e1ec2183f0b63d
Jose <jose.benitez@cern.ch>
2022-11-09 08:19:04 -0700

    pcc stat unc. for mu-scan fill

--- PCCAnalysis/plots/plotAfterglowModel.C
+++ PCCAnalysis/plots/plotAfterglowModel.C
@@ -5,6 +5,7 @@
 // https://github.com/cms-sw/cmssw/blob/master/Calibration/LumiAlCaRecoProducers/plugins/CorrPCCProducer.cc
 // type2_a_ * exp(-(float(bx) - 1) * type2_b_)
 #define formula "[0]*exp(-(x - 1) * [1])"
+
 //params:
 // https://github.com/cms-sw/cmssw/blob/master/Calibration/LumiAlCaRecoProducers/python/CorrPCCProducer_cfi.py
 // https://github.com/cms-sw/cmssw/blob/master/Calibration/LumiAlCaRecoProducers/python/AlcaLumiPCCHarvester_cff.py
@@ -12,15 +13,16 @@
 #define type2_a 0.00072
 #define type2_b 0.014
 
+///Type 1 from 2018 root files
+// /eos/user/b/benitezj/BRIL/PCC/AlCaPCCRandom/AlCaLumiPixels_AlCaPCCRandom/Run2018B_dynamicVeto/317626.root
+#define Type1Frac 0.024 
+
+
+void plotAfterglowModel(){
 
 #define modelrange 3600
 #define range 25
 
-#define ncolliding 150
-#define interval 22
-
-void plotAfterglowModel(){
-  
   TF1 F("F",formula,0.5,modelrange+0.5);
   F.SetParameter(0,type2_a);
   F.SetParameter(1,type2_b);
@@ -36,8 +38,8 @@ void plotAfterglowModel(){
   TCanvas C;
   C.SetLeftMargin(0.15);
 
-
-  /////////Plot of the per bunch Afterglow model
+  /////////////////////////////////////////////////
+  ///////// Plot of the per bunch Afterglow model
   C.Clear();
   HF.Draw("hist");
   F.Draw("lsame");
@@ -46,14 +48,20 @@ void plotAfterglowModel(){
   C.Print("plotAfterglowModel.png");
 
 
-  //Plot of the sum of 150 equally spaced bunches
+  
+  ////////////////////////////////////////////////
+  /////// Plot of the sum of 150 equally spaced bunches
+///for vdM fill
+#define ncolliding 150
+#define interval 22
+
   TH1F HModelSumOrbit("HModelSumOrbit","",range,0.5,range+0.5);
   HModelSumOrbit.SetTitle("Afterglow for vdM fill");
   HModelSumOrbit.GetYaxis()->SetTitle(TString("Model Sum (n=")+ncolliding+", interval="+interval+")");
   HModelSumOrbit.GetXaxis()->SetTitle("bunch number");
   HModelSumOrbit.SetStats(0);
 
-  for(int i=0;i<150;i++){
+  for(int i=0;i<ncolliding;i++){
     for(int b=1;b<=range;b++)
       HModelSumOrbit.AddBinContent(b,F.Eval(i*interval+b));
   }
@@ -62,54 +70,76 @@ void plotAfterglowModel(){
   C.Print("plotAfterglowModelSum.png");  
 
 
+
+  
   /////////////////////////////////////////////////////////////
-  ///Analysis of a train with 1800 collding bunches
+  /////// Analysis of Afterglow uncertainty for 36 bunch trains
   //https://lpc.web.cern.ch/fillingSchemes/2022/candidates/25ns_1815b_1802_1443_1542_180bpi_14inj_3INDIVs.csv
   //looks like 18 colliding bunches per train
   int NBPTrain=36;
-  int NBPGap=7;
-  
+  int NBPGap=8;
+  int NBTotal=NBPTrain*2+NBPGap*2;
+    
   //1. Afterglow is normalized on the colliding bunches.
   //2. Unc is driven by sqrt(N) on colliding bunches
   //3. nominal rate for Randoms=400hz, nominal PCC per pp=100, integration time, pileup 
   int RDTriggerRate=400;
-  float RDTriggerRatePerB=(float)RDTriggerRate/NBX;
+  float RDTriggerRatePerB=(float)RDTriggerRate/NBX;//Trigger is divided by full orbit
   int PCCPerPP=100;
-  float LSTime=23.31*50;
+  float LSTime=23.31*50;  // scale by number of lumisections
   int Pileup=40;
-  float Type1Frac=0.024;///eos/user/b/benitezj/BRIL/PCC/AlCaPCCRandom/AlCaLumiPixels_AlCaPCCRandom/Run2018B_dynamicVeto/317626.root
   
+ 
   float collisionCount=PCCPerPP*Pileup*RDTriggerRatePerB*LSTime;
   cout<<"collisionCount= "<<collisionCount<<endl;
   
-  TH1F HTrainSignal("HTrainSignal","",NBPTrain+NBPGap,0.5,NBPTrain+NBPGap+0.5);
-  for(int i=1;i<=NBPTrain;i++)
-    HTrainSignal.Fill(i,collisionCount);
-
-  TH1F HTrainBKGType1("HTrainBKGType1","",NBPTrain+NBPGap,0.5,NBPTrain+NBPGap+0.5);
-  for(int i=1;i<=NBPTrain;i++)
-    HTrainBKGType1.Fill(i+1,collisionCount*Type1Frac);
+  TH1F HTrainSignal("HTrainSignal","",NBTotal,0.5,NBTotal+0.5);
+  TH1F HTrainBKGType1("HTrainBKGType1","",NBTotal,0.5,NBTotal+0.5);
+  TH1F HTrainBKGType2("HTrainBKGType2","",NBTotal,0.5,NBTotal+0.5);
+  for(int i=1;i<=NBTotal;i++)
+    if(i<=NBPTrain || (i>NBPTrain+NBPGap && i<=(NBPTrain*2+NBPGap))){
+      HTrainSignal.Fill(i,collisionCount);
+      HTrainBKGType1.Fill(i+1,collisionCount*Type1Frac);
+      for(int j=i+1;j<=NBTotal;j++)
+	HTrainBKGType2.Fill(j,collisionCount*F.Eval(j-i));
+     }
   
-  TH1F HTrainBKGType2("HTrainBKGType2","",NBPTrain+NBPGap,0.5,NBPTrain+NBPGap+0.5);
-  for(int i=1;i<=NBPTrain;i++)
-    for(int j=i+1;j<=(NBPTrain+NBPGap);j++)
-      HTrainBKGType2.Fill(j,collisionCount*F.Eval(j-i));
-
-  TH1F HTrainBKG("HTrainBKG","",NBPTrain+NBPGap,0.5,NBPTrain+NBPGap+0.5);
+  TH1F HTrainBKG("HTrainBKG","",NBTotal,0.5,NBTotal+0.5);
   HTrainBKG.Add(&HTrainBKGType1);
   HTrainBKG.Add(&HTrainBKGType2);
   HTrainBKG.SetLineColor(4);
-  
-  TH1F HTrainBKGUnc("HTrainBKGUnc","",NBPTrain+NBPGap,0.5,NBPTrain+NBPGap+0.5);
+
+
+
+  TH1D * HTrainBKGUncBCID[NBTotal];
+  for(int i=0;i<NBTotal;i++)
+    HTrainBKGUncBCID[i] = new TH1D(TString("HTrainBKGUncBCID")+i,"",NBTotal,0.5,NBTotal+0.5);
+  for(int i=1;i<=NBTotal;i++)
+    if(i<=NBPTrain || (i>(NBPTrain+NBPGap) && i<=(NBPTrain*2+NBPGap))){
+      for(int j=i+1;j<=(NBTotal);j++){
+	double unc = 0;
+	if(j==i+1) unc = sqrt(collisionCount)*(Type1Frac + F.Eval(j-i));
+	if(j>i+1) unc = sqrt(collisionCount)*F.Eval(j-i);
+	HTrainBKGUncBCID[i-1]->Fill(j,unc);
+      }
+    }
+  TH1D HTrainBKGUnc("HTrainBKGUnc","",NBTotal,0.5,NBTotal+0.5);
   HTrainBKGUnc.SetLineColor(3);
-  for(int i=1;i<=NBPTrain;i++)
-    HTrainBKGUnc.Fill(i+1,sqrt(collisionCount)*Type1Frac);
-  for(int i=1;i<=NBPTrain;i++)
-    for(int j=i+1;j<=(NBPTrain+NBPGap);j++)
-      HTrainBKGUnc.Fill(j,sqrt(collisionCount)*F.Eval(j-i));
+  for(int i=1;i<=NBTotal;i++)
+    if(i<=NBPTrain || (i>(NBPTrain+NBPGap) && i<=(NBPTrain*2+NBPGap))){
+      for(int j=i+1;j<=(NBTotal);j++){
+	double unc = 0;
+	if(j==i+1) unc = sqrt(collisionCount)*(Type1Frac + F.Eval(j-i));
+	if(j>i+1) unc = sqrt(collisionCount)*F.Eval(j-i);
+	HTrainBKGUnc.Fill(j,unc*unc);
+      }
+    }  
+  for(int i=1;i<=NBTotal;i++)
+    HTrainBKGUnc.SetBinContent(i,sqrt(HTrainBKGUnc.GetBinContent(i)));
+
 
   
-  HTrainSignal.GetYaxis()->SetRangeUser(0.1,1.1*collisionCount);
+  HTrainSignal.GetYaxis()->SetRangeUser(0.01,1.1*collisionCount);
   HTrainSignal.GetYaxis()->SetTitle("Cluster Count");
   HTrainSignal.GetXaxis()->SetTitle("bx");
   HTrainSignal.SetMarkerStyle(8);
@@ -118,53 +148,77 @@ void plotAfterglowModel(){
   C.Clear();
   C.SetLogy(1);
   HTrainSignal.Draw("histp");
+  //HTrainBKGType1.Draw("histsame");
+  //HTrainBKGType2.Draw("histsame");
   HTrainBKG.Draw("histsame");
-  //HTrainBKGUnc.Draw("histsame");
+  HTrainBKGUnc.Draw("histsame");
   C.Print("plotAfterglowModel_TrainSignalBKGCount.png");  
+
+
   
-  TH1F HTrain("HTrain","",NBPTrain+NBPGap,0.5,NBPTrain+NBPGap+0.5);
+  TH1F HTrain("HTrain","",NBTotal,0.5,NBTotal+0.5);
   HTrain.Add(&HTrainSignal);
   HTrain.Add(&HTrainBKG);
   HTrain.SetMarkerStyle(8);
   HTrain.SetMarkerSize(0.7);
   HTrain.SetStats(0);
-  for(int i=1;i<=(NBPTrain+NBPGap);i++)
-    HTrainBKGUnc.Fill(i+1,sqrt(collisionCount)*Type1Frac);
+  HTrain.SetLineColor(4);
   C.Clear();
   C.SetLogy(1);
-  HTrain.GetYaxis()->SetRangeUser(0.1,1.1*collisionCount);
+  HTrain.GetYaxis()->SetRangeUser(0.01,1.1*collisionCount);
   HTrain.GetYaxis()->SetTitle("Cluster Count");
   HTrain.GetXaxis()->SetTitle("bx");
   HTrain.Draw("hist");
   HTrainSignal.Draw("histpsame");
-  C.Print("plotAfterglowModel_Train.png");
+  C.Print("plotAfterglowModel_TotalTrainSignal.png");
 
-  TH1F HTrainCorr("HTrainCorr","",NBPTrain+NBPGap,0.5,NBPTrain+NBPGap+0.5);
-  TH1F HTrainCorrUnc("HTrainCorrUnc","",NBPTrain+NBPGap,0.5,NBPTrain+NBPGap+0.5);
-  for(int i=1;i<=(NBPTrain+NBPGap);i++){
-    HTrainCorr.SetBinContent(i,HTrainSignal.GetBinContent(i)/HTrain.GetBinContent(i));
-    HTrainCorr.SetBinError(i,HTrainBKGUnc.GetBinContent(i)/HTrain.GetBinContent(i));
 
-    HTrainCorrUnc.SetBinContent(i,HTrainBKGUnc.GetBinContent(i)/collisionCount);
+
+ 
+  C.Clear();
+  C.SetLogy(1);
+  HTrainBKG.GetYaxis()->SetRangeUser(0.01,2*HTrainBKG.GetMaximum());
+  HTrainBKG.GetYaxis()->SetTitle("Afterglow Cluster Count");
+  HTrainBKG.GetXaxis()->SetTitle("bx");
+  HTrainBKG.SetStats(0);
+  HTrainBKG.Draw("hist");
+  HTrainBKGUnc.Draw("histsame");
+  //for(int i=0;i<NBTotal;i++)
+  //HTrainBKGUncBCID[i]->Draw("histsame");
+  C.Print("plotAfterglowModel_TrainBKGUnc.png");
+
+
+  
+  
+  TH1F HTrainCorr("HTrainCorr","",NBTotal,0.5,NBTotal+0.5);
+  for(int i=1;i<=NBTotal;i++){
+    float c=HTrainSignal.GetBinContent(i)/HTrain.GetBinContent(i);
+    HTrainCorr.SetBinContent(i,c);
+    HTrainCorr.SetBinError(i,c*HTrainBKGUnc.GetBinContent(i)/HTrain.GetBinContent(i));
   }
+  
+  TH1F HTrainCorrUnc("HTrainCorrUnc","",NBTotal,0.5,NBTotal+0.5);
+  for(int i=1;i<=NBTotal;i++)
+    HTrainCorrUnc.SetBinContent(i,HTrainCorr.GetBinError(i));
+  
 
   HTrainCorr.SetStats(0);
-  HTrainCorr.GetYaxis()->SetRangeUser(0.95,1.01);
+  HTrainCorr.GetYaxis()->SetRangeUser(0.92,1.01);
   HTrainCorr.GetYaxis()->SetTitle("Afterglow Correction");
   HTrainCorr.GetXaxis()->SetTitle("bx");
   HTrainCorr.SetMarkerStyle(8);
-  HTrainCorr.SetMarkerSize(0.4);
+  HTrainCorr.SetMarkerSize(0.5);
   C.Clear();
   C.SetLogy(0);
   HTrainCorr.Draw("histlpe");
   C.Print("plotAfterglowModel_TrainCorr.png");
 
   HTrainCorrUnc.SetStats(0);
-  HTrainCorrUnc.GetYaxis()->SetRangeUser(0,0.005);
+  HTrainCorrUnc.GetYaxis()->SetRangeUser(0,0.002);
   HTrainCorrUnc.GetYaxis()->SetTitle("Afterglow Correction Uncertainty");
   HTrainCorrUnc.GetXaxis()->SetTitle("bx");
   HTrainCorrUnc.SetMarkerStyle(8);
-  HTrainCorrUnc.SetMarkerSize(0.4);
+  HTrainCorrUnc.SetMarkerSize(0.5);
   C.Clear();
   C.SetLogy(0);
   HTrainCorrUnc.Draw("histp");
