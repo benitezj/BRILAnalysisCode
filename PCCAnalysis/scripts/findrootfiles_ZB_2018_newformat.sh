5c1d2d0ac8827c4c4b765bc9941b618527291e15
Ashish Sehrawat <50741390+ashishadscft@users.noreply.github.com>
2023-02-14 23:45:42 -0700

    find root files AlCaLumiPixels new format

---
+++ PCCAnalysis/scripts/findrootfiles_ZB_2018_newformat.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set the EOS area path and the target directory for the output folders
+EOS_PATH="/eos/cms/store/data/Run2018A/AlCaLumiPixels/ALCARECO/AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/"
+OUTPUT_DIR="/afs/cern.ch/user/a/asehrawa/PCC_27Oct2022_UL2018/CMSSW_10_6_30/src/zerobias_27Oct2022_Run2018"
+
+# Find all root files in the EOS area and loop over them
+find "$EOS_PATH" -name "*.root" -type f | while read root_file; do
+    # Extract the filename without the path or extension
+    filename=$(basename "$root_file" .root)
+
+    # Create a folder with the filename in the output directory
+    mkdir -p "$OUTPUT_DIR/$filename"
+
+    # Copy the root file to the folder
+    cp "$root_file" "$OUTPUT_DIR/$filename"
+done
