15de55a612f6914d13330370e98891e8ad4f0c75
Ashish Sehrawat <50741390+ashishadscft@users.noreply.github.com>
2023-02-15 00:36:33 -0700

    update

--- PCCAnalysis/scripts/findrootfiles_ZB_2018_newformat.sh
+++ PCCAnalysis/scripts/findrootfiles_ZB_2018_newformat.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Set the EOS area path and the target directory for the output files
 EOS_PATH="/eos/cms/store/data/Run2018B/AlCaLumiPixels/ALCARECO/AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/2520000/"
 OUTPUT_DIR="/afs/cern.ch/user/a/asehrawa/PCC_27Oct2022_UL2018/CMSSW_10_6_30/src/zerobias_27Oct2022_Run2018B"
