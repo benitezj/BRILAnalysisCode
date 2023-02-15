8de61550df22a70d8125c1d232a54881db5a4c21
Ashish Sehrawat <50741390+ashishadscft@users.noreply.github.com>
2023-02-15 00:03:59 -0700

    update

--- PCCAnalysis/scripts/findrootfiles_ZB_2018_newformat.sh
+++ PCCAnalysis/scripts/findrootfiles_ZB_2018_newformat.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set the EOS area path and the target directory for the output folders
-EOS_PATH="/eos/cms/store/data/Run2018A/AlCaLumiPixels/ALCARECO/AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/2550000/"
-OUTPUT_DIR="/afs/cern.ch/user/a/asehrawa/PCC_27Oct2022_UL2018/CMSSW_10_6_30/src/zerobias_27Oct2022_Run2018"
+EOS_PATH="/eos/cms/store/data/Run2018B/AlCaLumiPixels/ALCARECO/AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/2520000/"
+OUTPUT_DIR="/afs/cern.ch/user/a/asehrawa/PCC_27Oct2022_UL2018/CMSSW_10_6_30/src/zerobias_27Oct2022_Run2018B"
 
 # Find all root files in the EOS area and loop over them
 find "$EOS_PATH" -name "*.root" -type f | while read root_file; do
