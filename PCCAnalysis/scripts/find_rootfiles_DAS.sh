97def956e3d3f887c0b56104367fb9f7f277c6c6
Ashish Sehrawat <50741390+ashishadscft@users.noreply.github.com>
2023-02-16 14:14:49 -0700

    not working

---
+++ PCCAnalysis/scripts/find_rootfiles_DAS.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                                                                                                                                                        
+
+# Set the path to the directory containing the root files                                                                                                          
+DIRECTORY="/store/data/Run2018B/AlCaLumiPixels/ALCARECO/AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/2520000/"
+
+# Use the "xrdfs" command to list the files in the directory                                                                                                       
+# and filter the output to show only the files ending with ".root"                                                                                                 
+FILES=$(xrdfs root://cms-xrd-global.cern.ch ls -l $DIRECTORY | awk '{print $5, $9}' | grep -E '\.root$' | awk '{print $2}')
+
+# Loop through the files and print their names                                                                                                                     
+for FILE in $FILES
+do
+    echo $FILE
+done
