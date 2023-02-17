03ae89658350e75192f7bc33c49af2e021c47028
Ashish Sehrawat <50741390+ashishadscft@users.noreply.github.com>
2023-02-16 21:12:17 -0700

    stuck!

--- PCCAnalysis/scripts/find_rootfiles_DAS.sh
+++ PCCAnalysis/scripts/find_rootfiles_DAS.sh
@@ -1,11 +1,11 @@
 #!/bin/bash                                                                                                                                                        
 
 # Set the path to the directory containing the root files                                                                                                          
-DIRECTORY="/store/data/Run2018B/AlCaLumiPixels/ALCARECO/AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/2520000/"
+DIRECTORY="/store/test/xrootd/T1_US_FNAL/store/data/Run2018B/AlCaLumiPixels/ALCARECO/AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/2530000/"
 
 # Use the "xrdfs" command to list the files in the directory                                                                                                       
 # and filter the output to show only the files ending with ".root"                                                                                                 
-FILES=$(xrdfs root://cms-xrd-global.cern.ch/ ls -l $DIRECTORY | awk '{print $5, $9}' | grep -E '\.root$' | awk '{print $2}')
+FILES=$(xrdfs root://cmsxrootd.fnal.gov/ ls -l $DIRECTORY | awk '{print $5, $9}' | grep -E '\.root$' | awk '{print $2}')
 
 # Loop through the files and print their names                                                                                                                     
 for FILE in $FILES
