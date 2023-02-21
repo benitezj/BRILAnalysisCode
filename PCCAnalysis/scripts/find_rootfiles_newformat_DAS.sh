a5fef852f3e3e7c09483450a762a187264881217
Ashish Sehrawat <50741390+ashishadscft@users.noreply.github.com>
2023-02-21 11:57:42 -0700

    update

--- PCCAnalysis/scripts/find_rootfiles_newformat_DAS.sh
+++ PCCAnalysis/scripts/find_rootfiles_newformat_DAS.sh
@@ -1,12 +1,37 @@
 #!/bin/bash                                                                                                                                            
 
 #To create the input file, run the following command in terminal for period A,B and C
-#dasgoclient -query="file dataset=/AlCaLumiPixels/Run2018A-AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/ALCARECO instance=prod/global | grep file.name | grep '.root'" > Run2018A.txt
-#dasgoclient -query="file dataset=/AlCaLumiPixels/Run2018B-AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/ALCARECO instance=prod/global | grep file.name | grep '.root'" > Run2018B.txt
-#dasgoclient -query="file dataset=/AlCaLumiPixels/Run2018C-AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/ALCARECO instance=prod/global | grep file.name | grep '.root'" > Run2018C.txt
+#dasgoclient -query="file dataset=/AlCaLumiPixels/Run2018A-AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/ALCARECO instance=prod/global | grep file.name | grep '.root'" 
+#dasgoclient -query="file dataset=/AlCaLumiPixels/Run2018B-AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/ALCARECO instance=prod/global | grep file.name | grep '.root'" 
+#dasgoclient -query="file dataset=/AlCaLumiPixels/Run2018C-AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/ALCARECO instance=prod/global | grep file.name | grep '.root'" 
+
+echo "Please select the data period: "
+echo "1. Period A (Run2018A)"
+echo "2. Period B (Run2018B)"
+echo "3. Period C (Run2018C)"
+
+read period_choice
+
+case $period_choice in
+    1)
+        input_file="Run2018A.txt"
+        ;;
+    2)
+        input_file="Run2018B.txt"
+        ;;
+    3)
+        input_file="Run2018C.txt"
+        ;;
+    *)
+        echo "Invalid choice. Please enter 1, 2, or 3."
+        exit 1
+        ;;
+esac
+
+echo "Selected input file: $input_file"
 
 # Get the input file name from user                                                                                                                    
-echo "Enter the name of the input file:"
+#echo "Enter the name of the input file:"
 read input_file
 
 # Check if the input file exists                                                                                                                       
