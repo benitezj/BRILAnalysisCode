96d4f1054416a97c938cda298b6d870b3309fcbc
ashish <asehrawat1991@gmail.com>
2023-02-23 23:53:32 +0100

    compare TTreey

--- PCCAnalysis/python/compareTTreebranches.py
+++ PCCAnalysis/python/compareTTreebranches.py
@@ -1,34 +1,28 @@
 import ROOT
 
-# Open the first file
-file1 = ROOT.TFile.Open("root://cms-xrd-global.cern.ch//store/data/Run2018B/AlCaLumiPixels/ALCARECO/AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/60000/980BE645-E9A7-7145-B26D-5459E678C3F0.root")
+# Open the first file and get the list of TTree names
+file1 = ROOT.TFile.Open("file1.root", "READ")
+trees1 = [key.GetName() for key in file1.GetListOfKeys() if key.GetClassName() == "TTree"]
 
-# Get the list of branch names for file1
-branches_file1 = []
-for key in file1.GetListOfKeys():
-    if key.GetClassName() == "TTree":
-        tree = key.ReadObj()
-        for branch in tree.GetListOfBranches():
-            branches_file1.append(branch.GetName())
+# Open the second file and get the list of TTree names
+file2 = ROOT.TFile.Open("file2.root", "READ")
+trees2 = [key.GetName() for key in file2.GetListOfKeys() if key.GetClassName() == "TTree"]
 
-# Open the second file
-file2 = ROOT.TFile.Open("file2.root")
+# Print the common TTree names between the two files
+print("Common TTree names:")
+for tree in set(trees1).intersection(trees2):
+    print(tree)
 
-# Get the list of branch names for file2
-branches_file2 = []
-for key in file2.GetListOfKeys():
-    if key.GetClassName() == "TTree":
-        tree = key.ReadObj()
-        for branch in tree.GetListOfBranches():
-            branches_file2.append(branch.GetName())
+# Print the TTree names in file1 that are not in file2
+print("TTree names in file1 that are not in file2:")
+for tree in set(trees1).difference(trees2):
+    print(tree)
 
-# Compare the lists of branch names
-if len(branches_file1) != len(branches_file2):
-    print("The two files have different numbers of branches")
-else:
-    for i in range(len(branches_file1)):
-        if branches_file1[i] != branches_file2[i]:
-            print("The two files have different branch names")
-            break
-    else:
-        print("The two files have the same branch names")
+# Print the TTree names in file2 that are not in file1
+print("TTree names in file2 that are not in file1:")
+for tree in set(trees2).difference(trees1):
+    print(tree)
+
+# Close the files
+file1.Close()
+file2.Close()
