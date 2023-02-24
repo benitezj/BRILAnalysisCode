197f0bddf10425caf9b30b9066bef1e33fff5096
ashish <asehrawat1991@gmail.com>
2023-02-24 14:42:41 +0100

    compare TTree branches names

--- PCCAnalysis/python/compareTTreebranches.py
+++ PCCAnalysis/python/compareTTreebranches.py
@@ -1,11 +1,11 @@
 import ROOT
 
 # Open the first file and get the list of TTree names
-file1 = ROOT.TFile.Open("file1.root", "READ")
+file1 = ROOT.TFile.Open("root://cms-xrd-global.cern.ch//store/data/Run2018B/AlCaLumiPixels/ALCARECO/AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/60000/980BE645-E9A7-7145-B26D-5459E678C3F0.root", "READ")
 trees1 = [key.GetName() for key in file1.GetListOfKeys() if key.GetClassName() == "TTree"]
 
 # Open the second file and get the list of TTree names
-file2 = ROOT.TFile.Open("file2.root", "READ")
+file2 = ROOT.TFile.Open("root://cms-xrd-global.cern.ch//store/data/Run2018B/AlCaLumiPixels/ALCARECO/AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/2520000/288996FE-3259-0945-8BEA-FD6F56B348CB.root", "READ")
 trees2 = [key.GetName() for key in file2.GetListOfKeys() if key.GetClassName() == "TTree"]
 
 # Print the common TTree names between the two files
