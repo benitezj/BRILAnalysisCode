46b043e409894dc36aeb6030012126c3c80bfd40
Ashish Sehrawat <50741390+ashishadscft@users.noreply.github.com>
2023-03-11 01:06:29 -0700

    compare datasets (size, events, ls)

---
+++ PCCAnalysis/scripts/compare_datasets_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash                                                                                                                                                 
+
+export LC_ALL=C
+export LANG=en_US.UTF-8
+
+# Define the two datasets to compare                                                                                                                        
+dataset1="/AlCaLumiPixels/Run2018A-AlCaPCCZeroBias-PromptReco-v3/ALCARECO"
+dataset2="/AlCaLumiPixels/Run2018A-AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/ALCARECO"
+
+# Function to print information for a given dataset                                                                                                         
+print_info() {
+  dataset=$1
+  echo "Information for dataset: $dataset"
+  dataset_info=$(dasgoclient -query="dataset=$dataset" -json | grep -E 'size|nevents|num_lumi' | sed 's/,$//')
+  size=$(echo $dataset_info | jq -r '.dataset[0].size')
+  nevents=$(echo $dataset_info | jq -r '.dataset[0].nevents')
+  num_lumi=$(echo $dataset_info | jq -r '.dataset[0].num_lumi')
+  size_per_event=$(echo "scale=2; $size / $nevents" | bc)
+  size_per_lumi=$(echo "scale=2; $size / $num_lumi" | bc)
+
+  echo "Number of events: $nevents"
+  echo "Dataset size: $size bytes"
+  echo "Number of lumi sections: $num_lumi"
+  echo "Dataset size per event: $size_per_event bytes"
+  echo "Dataset size per lumi section: $size_per_lumi bytes"
+}
+
+# Call the function for both datasets                                                                                                                       
+print_info $dataset1
+print_info $dataset2
