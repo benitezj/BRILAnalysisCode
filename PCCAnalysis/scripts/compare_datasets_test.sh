60a20944de05e458488cb111764c3eb85a72bbf5
Ashish Sehrawat <50741390+ashishadscft@users.noreply.github.com>
2023-03-12 22:50:26 -0700

    updated

--- PCCAnalysis/scripts/compare_datasets_test.sh
+++ PCCAnalysis/scripts/compare_datasets_test.sh
@@ -1,11 +1,18 @@
-#!/bin/bash                                                                                                                                                 
+#!/bin/bash                                                                                                                                                                                                   
 
 export LC_ALL=C
 export LANG=en_US.UTF-8
-                                                                                                                        
+
+# Define the two datasets to compare                                                                                                                                                                          
 dataset1="/AlCaLumiPixels/Run2018A-AlCaPCCZeroBias-PromptReco-v3/ALCARECO"
-dataset2="/AlCaLumiPixels/Run2018A-AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/ALCARECO"
-                                                                                                         
+dataset2="/AlCaLumiPixels/Run2018C-AlCaPCCZeroBias-PromptReco-v3/ALCARECO"
+dataset3="/AlCaLumiPixels/Run2018D-AlCaPCCZeroBias-PromptReco-v2/ALCARECO"
+dataset4="/AlCaLumiPixels/Run2018A-AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/ALCARECO"
+dataset5="/AlCaLumiPixels/Run2018B-AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/ALCARECO"
+dataset6="/AlCaLumiPixels/Run2018C-AlCaPCCZeroBias-27Oct2022_UL2018_PCC-v1/ALCARECO"
+
+
+# Function to print information for a given dataset                                                                                                                                                           
 print_info() {
   dataset=$1
   echo "Information for dataset: $dataset"
@@ -22,6 +29,11 @@ print_info() {
   echo "Dataset size per event: $size_per_event bytes"
   echo "Dataset size per lumi section: $size_per_lumi bytes"
 }
-                                                                                                                       
+
+# Call the function for both datasets                                                                                                                                                                         
 print_info $dataset1
 print_info $dataset2
+print_info $dataset3
+print_info $dataset4
+print_info $dataset5
+print_info $dataset6
