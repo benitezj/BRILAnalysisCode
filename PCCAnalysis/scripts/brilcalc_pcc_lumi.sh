bc72d1d7dfd795bf49ce115ff03f7f06ac2fd012
Ashish Sehrawat <50741390+ashishadscft@users.noreply.github.com>
2023-09-17 20:07:07 +0530

    2022 brilcalc pcc hfet lumi

--- PCCAnalysis/scripts/brilcalc_pcc_lumi.sh
+++ PCCAnalysis/scripts/brilcalc_pcc_lumi.sh
@@ -1,14 +1,18 @@
-#!/bin/bash                                                                                                                                                                                                                          
+#!/bin/bash                                                                                                                                                                                                                         \
+                                                                                                                                                                                                                                     
 
 OUTPUT="combined_lumi.csv"
 TEMP_OUTPUT="temp_lumi.csv"
 
 echo -e "Run    LumiSection    Luminosity" > $OUTPUT
 
-for RUN in $(seq 355862 362760); do
-#for RUN in $(seq 355862 355868); do                                                                                                                                                                                                 
-    brilcalc lumi -c offline --datatag 23v1 --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_pcc.json -u /fb --byls --output-style csv --type pxl -r $RUN | \
-    awk -F, 'NR>1 {split($1,run,":"); split($2,ls,":"); print run[1]"    "ls[1]"    "$6}' >> $TEMP_OUTPUT
+#LAST_RUN=355870                                                                                                                                                                                                                     
+LAST_RUN=362760
+
+for RUN in $(seq 355862 $LAST_RUN); do
+    echo "Processing RUN: $RUN"
+    brilcalc lumi -c offline --datatag 23v1 --normtag /cvmfs/cms-bril.cern.ch/cms-lumi-pog/Normtags/normtag_pcc.json -u /fb --byls --output-style csv --type pxl -r $RUN | awk -F, 'NR>1 {split($1,run,":"); split($2,ls,":"); print\
+ run[1]"    "ls[1]"    "$6}' >> $TEMP_OUTPUT
 done
 
 sed -e '/Run    LumiSection    Luminosity/d' \
@@ -19,3 +23,5 @@ sed -e '/Run    LumiSection    Luminosity/d' \
     -e '/^#/d' $TEMP_OUTPUT >> $OUTPUT
 
 rm $TEMP_OUTPUT
+
+echo "Processed up to the last run number: $LAST_RUN. Exiting..."
